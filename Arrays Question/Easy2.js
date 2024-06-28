581c50a2b2c5f28d206210c1ff1eb7cbb76f0c1b
mohammad-anash <anash40678910@gmail.com>
2024-06-28 12:35:32 +0530

    Updated Files

--- Arrays Question/Easy2.js
+++ Arrays Question/Easy2.js
@@ -217,9 +217,7 @@ function intersection(arr) {
 // 115 => distribute candies
 
 // Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
-
 // The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
-
 // Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
 
 // Example 1:
@@ -241,7 +239,104 @@ function intersection(arr) {
 function distributeCandies(nums) {
   return Math.min(Array.from(new Set(nums)).length, nums.length / 2);
 }
-console.log(distributeCandies([1, 1, 2, 2, 3, 3]));
-console.log(distributeCandies([1, 1, 2, 3]));
-console.log(distributeCandies([6, 6, 6, 6]));
-console.log(distributeCandies([1, 1, 1, 2, 2, 2, 3, 3, 3]));
+
+// console.log(distributeCandies([1, 1, 2, 2, 3, 3]));
+// console.log(distributeCandies([1, 1, 2, 3]));
+// console.log(distributeCandies([6, 6, 6, 6]));
+// console.log(distributeCandies([1, 1, 1, 2, 2, 2, 3, 3, 3]));
+
+// 116 => fine the middle Index
+
+// Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+// A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+// If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+//
+// Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+// Example 1:
+
+// Input: nums = [2,3,-1,8,4]
+// Output: 3/
+// Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+// The sum of the numbers after index 3 is: 4 = 4
+// Example 2:
+
+// Input: nums = [1,-1,4]
+// Output: 2
+// Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+// The sum of the numbers after index 2 is: 0
+// Example 3:
+
+// Input: nums = [2,5]
+// Output: -1
+// Explanation: There is no valid middleIndex.
+
+function findMiddleIndex(arr) {
+  let totalSum = arr.reduce((a, b) => a + b, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let rightSum = totalSum - leftSum - arr[i];
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+
+    leftSum += arr[i];
+  }
+
+  return -1;
+}
+
+// console.log(findMiddleIndex([2, 3, -1, 8, 4])); // Output: 3
+// console.log(findMiddleIndex([1, -1, 4])); // Output: 2
+// console.log(findMiddleIndex([2, 5])); // Output: -1
+
+// 117 => app operation to an array
+
+// You are given a 0-indexed array nums of size n consisting of non-negative integers.
+
+// You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
+
+// If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
+// After performing all the operations, shift all the 0's to the end of the array.
+
+// For example, the array [1,0,2,0,0,1] after shifting all its 0's to the end, is [1,2,1,0,0,0].
+// Return the resulting array.
+
+// Note that the operations are applied sequentially, not all at once.
+
+// Example 1:
+
+// Input: nums = [1,2,2,1,1,0]
+// Output: [1,4,2,0,0,0]
+// Explanation: We do the following operations:
+// - i = 0: nums[0] and nums[1] are not equal, so we skip this operation.
+// - i = 1: nums[1] and nums[2] are equal, we multiply nums[1] by 2 and change nums[2] to 0. The array becomes [1,4,0,1,1,0].
+// - i = 2: nums[2] and nums[3] are not equal, so we skip this operation.
+// - i = 3: nums[3] and nums[4] are equal, we multiply nums[3] by 2 and change nums[4] to 0. The array becomes [1,4,0,2,0,0].
+// - i = 4: nums[4] and nums[5] are equal, we multiply nums[4] by 2 and change nums[5] to 0. The array becomes [1,4,0,2,0,0].
+// After that, we shift the 0's to the end, which gives the array [1,4,2,0,0,0].
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: [1,0]
+// Explanation: No operation can be applied, we just shift the 0 to the end.
+
+function applyOperation(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = nums[i] * 2;
+      nums[i + 1] = nums[i + 1] = 0;
+    }
+  }
+  const collectZero = nums.filter((val) => val === 0);
+  const collectNonZero = nums.filter((val) => val !== 0);
+
+  return [...collectNonZero, ...collectZero];
+}
+
+console.log(applyOperation([1, 2, 2, 1, 1, 0]));
+console.log(applyOperation([0, 1]));
