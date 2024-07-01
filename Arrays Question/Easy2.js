495fbf5ad211b1e85222d4397d9fa2849b9cbdde
mohammad-anash <anash40678910@gmail.com>
2024-07-01 17:48:28 +0530

    Updated Files

--- Arrays Question/Easy2.js
+++ Arrays Question/Easy2.js
@@ -646,3 +646,48 @@ function search(nums, target) {
 
 // console.log(search([-1, 0, 3, 5, 9, 12], 9));
 // console.log(search([-1, 0, 3, 5, 9, 12], 2));
+
+// 128 => threee Consecutive Odds
+
+// Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+// Example 1:
+
+// Input: arr = [2,6,4,1]
+// Output: false
+// Explanation: There are no three consecutive odds.
+// Example 2:
+
+// Input: arr = [1,2,34,3,4,5,7,23,12]
+// Output: true
+// Explanation: [5,7,23] are three consecutive odds.
+
+function threeConsecutiveOdds(nums) {
+  if (nums.length < 3) return false;
+  for (let i = 0; i <= nums.length - 3; i++) {
+    if (nums[i] % 2 !== 0 && nums[i + 1] % 2 !== 0 && nums[i + 2] % 2 !== 0) {
+      return true;
+    }
+  }
+  return false;
+  3;
+}
+
+// console.log(threeConsecutiveOdds([2, 4, 6, 1]));
+// console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]));
+// console.log(threeConsecutiveOdds([1]));
+// console.log(threeConsecutiveOdds([1, 3]));
+
+// 129 => take gift fron the richest pile
+
+function pickGifts(arr, k) {
+  for (let i = 0; i < k; i++)
+    arr[arr.indexOf(Math.max(...arr))] = Math.floor(
+      Math.sqrt(Math.max(...arr))
+    );
+
+  return arr.reduce((sum, current) => sum + current, 0);
+}
+
+console.log(pickGifts([25, 64, 9, 4, 100], 4));
+console.log(pickGifts([1, 1, 1, 1], 4));
