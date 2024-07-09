5db6cd8cf8fbd2729838be6b8b259d8c336ac79e
mohammad-anash <anash40678910@gmail.com>
2024-07-09 16:07:31 +0530

    Updated Files

--- Arrays Question/Easy2.js
+++ Arrays Question/Easy2.js
@@ -1365,3 +1365,61 @@ function countPrefixAndSuffixPair(nums) {
 // console.log(countPrefixAndSuffixPair(["a", "aba", "ababa", "aa"]));
 // console.log(countPrefixAndSuffixPair(["pa", "papa", "ma", "mama"]));
 // console.log(countPrefixAndSuffixPair(["abab", "ab"]));
+
+// 143 => Build Array from Permutation
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// Example 1:
+
+// Input: nums = [0,2,1,5,3,4]
+// Output: [0,1,2,4,5,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+// = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+// = [0,1,2,4,5,3]
+// Example 2:
+
+// Input: nums = [5,0,1,2,3,4]
+// Output: [4,5,0,1,2,3]
+// Explanation: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+// = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+// = [4,5,0,1,2,3]
+
+// Second Time Solve
+
+const buildArray = (nums) => {
+  return nums.map((ans, index) => nums[nums[index]]);
+};
+// console.log(buildArray([0, 2, 1, 5, 3, 4]));
+// console.log(buildArray([5, 0, 1, 2, 3, 4]));
+
+// Second Time Solve
+
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+// Specifically, ans is the concatenation of two nums arrays.
+// Return the array ans.
+
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
+// Example 2:
+
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
+
+function getConcatenation(nums) {
+  return [...nums, ...nums];
+}
+
+console.log(getConcatenation([1, 2, 1]));
+console.log(getConcatenation([1, 3, 2, 1]));
