b7a9c00c2f7e3e28546c15738509088ee7f42f7d
mohammad-anash <anash40678910@gmail.com>
2024-07-13 23:20:57 +0530

    Updated Files

--- Arrays Question/Easy2.js
+++ Arrays Question/Easy2.js
@@ -1848,5 +1848,56 @@ function minMaxGame(nums) {
   }
   return nums[0];
 }
-console.log(minMaxGame([1, 3, 5, 2, 4, 8, 2, 2])); // Output: 1
-console.log(minMaxGame([3])); // Output: 3
+// console.log(minMaxGame([1, 3, 5, 2, 4, 8, 2, 2])); // Output: 1
+// console.log(minMaxGame([3])); // Output: 3
+
+// 152 =>
+
+// You are given a 0-indexed integer array nums. Rearrange the values of nums according to the following rules:
+
+// Sort the values at odd indices of nums in non-increasing order.
+// For example, if nums = [4,1,2,3] before this step, it becomes [4,3,2,1] after. The values at odd indices 1 and 3 are sorted in non-increasing order.
+// Sort the values at even indices of nums in non-decreasing order.
+// For example, if nums = [4,1,2,3] before this step, it becomes [2,1,4,3] after. The values at even indices 0 and 2 are sorted in non-decreasing order.
+// Return the array formed after rearranging the values of nums.
+
+// Example 1:
+
+// Input: nums = [4,1,2,3]
+// Output: [2,3,4,1]
+// Explanation:
+// First, we sort the values present at odd indices (1 and 3) in non-increasing order.
+// So, nums changes from [4,1,2,3] to [4,3,2,1].
+// Next, we sort the values present at even indices (0 and 2) in non-decreasing order.
+// So, nums changes from [4,1,2,3] to [2,3,4,1].
+// Thus, the array formed after rearranging the values is [2,3,4,1].
+// Example 2:
+//
+// Input: nums = [2,1]
+// Output: [2,1]
+// Explanation:
+// Since there is exactly one odd index and one even index, no rearrangement of values takes place.
+// The resultant array formed is [2,1], which is the same as the initial array.
+
+function sortEvenOdd(nums) {
+  const [evenNums, oddNums] = [[], []];
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) evenNums.push(nums[i]);
+    else oddNums.push(nums[i]);
+  }
+
+  evenNums.sort((a, b) => a - b);
+  oddNums.sort((a, b) => b - a);
+
+  const result = [];
+  let [evenIndex, oddIndex] = [0, 0]
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) result[i] = evenNums[evenIndex++];
+    else result[i] = oddNums[oddIndex++];
+  }
+
+  return result;
+}
+
+console.log(sortEvenOdd([4, 1, 2, 3])); // Output: [2, 3, 4, 1]
+console.log(sortEvenOdd([2, 1])); // Output: [2, 1]
