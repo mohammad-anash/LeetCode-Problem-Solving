47e900e878fdf0a3e24fb55b07bcbec03383a572
mohammad-anash <anash40678910@gmail.com>
2024-07-31 12:43:54 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -712,9 +712,89 @@ function sumOfIncryptedInt(nums) {
   return nums.reduce((acc, current) => {
     const inString = String(current);
     acc += +String(Math.max(...inString.split(""))).repeat(inString.length);
-    return acc
+    return acc;
   }, 0);
 }
 
 // console.log(sumOfIncryptedInt([1, 2, 3]));
 // console.log(sumOfIncryptedInt([10, 21, 31]));
+
+// 185 => Split the Array
+
+// You are given an integer array nums of even length. You have to split the array into two parts nums1 and nums2 such that:
+
+// nums1.length == nums2.length == nums.length / 2.
+// nums1 should contain distinct elements.
+// nums2 should also contain distinct elements.
+// Return true if it is possible to split the array, and false otherwise.
+
+// Example 1:
+
+// Input: nums = [1,1,2,2,3,4]
+// Output: true
+// Explanation: One of the possible ways to split nums is nums1 = [1,2,3] and nums2 = [1,2,4].
+// Example 2:
+
+// Input: nums = [1,1,1,1]
+// Output: false
+// Explanation: The only possible way to split nums is nums1 = [1,1] and nums2 = [1,1]. Both nums1 and nums2 do not contain distinct elements. Therefore, we return false.
+
+function isPossibleToSplit(nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] == nums[i + 2]) return false;
+  }
+  return true;
+}
+
+// console.log(isPossibleToSplit([1, 1, 2, 2, 3, 4]));
+// console.log(isPossibleToSplit([1, 1, 1, 1]));
+// console.log(isPossibleToSplit([3, 9, 5, 2, 7, 9, 8, 5]));
+// console.log(isPossibleToSplit([4, 3]));
+// console.log(isPossibleToSplit([6, 1, 3, 1, 1, 8, 9, 2]));
+
+// 186 =>  Find Resultant Array After Removing Anagrams
+
+// You are given a 0-indexed string array words, where words[i] consists of lowercase English letters.
+
+// In one operation, select any index i such that 0 < i < words.length and words[i - 1] and words[i] are anagrams, and delete words[i] from words. Keep performing this operation as long as you can select an index that satisfies the conditions.
+
+// Return words after performing all operations. It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original letters exactly once. For example, "dacb" is an anagram of "abdc".
+
+// Example 1:
+
+// Input: words = ["abba","baba","bbaa","cd","cd"]
+// Output: ["abba","cd"]
+// Explanation:
+// One of the ways we can obtain the resultant array is by using the following operations:
+// // - Since words[2] = "bbaa" and words[1] = "baba" are anagrams, we choose index 2 and delete words[2].
+// // // Now words = ["abba","baba","cd","cd"].
+// // // - Since words[1] = "baba" and words[0] = "abba" are anagrams, we choose index 1 and delete words[1].
+// // // Now words = ["abba","cd","cd"].
+// // // - Since words[2] = "cd" and words[1] = "cd" are anagrams, we choose index 2 and delete words[2].
+// // Now words = ["abba","cd"].
+// // We can no longer perform any operations, so ["abba","cd"] is the final answer.
+// Example 2:
+
+// Input: words = ["a","b","c","d","e"]
+// Output: ["a","b","c","d","e"]
+// Explanation:
+// No two adjacent strings in words are anagrams of each other, so no operations are performed.
+
+function removeAnagram(words) {
+  for (let i = 1; i < words.length; ) {
+    const [countWord, prevWord] = [
+      words[i].split("").sort().join(),
+      words[i - 1].split("").sort().join(),
+    ];
+    countWord === prevWord ? words.splice(i, 1) : i++;
+  }
+
+  return words;
+}
+
+// console.log(removeAnagram(["abba", "baba", "bbaa", "cd", "cd"]));
+// console.log(removeAnagram(["a", "b", "c", "d", "e"]));
+// console.log(removeAnagram(["a", "b", "a"]));
--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -2303,3 +2303,83 @@ function findTheDiffrence(s, t) {
 //     "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"
 //   )
 // );
+
+// 65 => Detect capital
+
+// We define the usage of capitals in a word to be right when one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// // All letters in this word are not capitals, like "leetcode".
+// // Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+
+// Example 1:
+
+// Input: word = "USA"
+// Output: true
+// Example 2:
+
+// Input: word = "FlaG"
+// Output: false
+
+function detectCapitalUse(s) {
+  const inCaptalize = s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+  if (s.toUpperCase() === s || s.toLowerCase() === s || s === inCaptalize)
+    return true;
+  else return false;
+}
+
+// console.log(detectCapitalUse("USA"));
+// console.log(detectCapitalUse("FlaG"));
+
+// 66 => Divide a String Into Groups of Size k
+
+// A string s can be partitioned into groups of size k using the following procedure:
+
+// The first group consists of the first k characters of the string, the second group consists of the next k characters of the string, and so on. Each character can be a part of exactly one group.
+// // For the last group, if the string does not have k characters remaining, a character fill is used to complete the group.
+// // Note that the partition is done so that after removing the fill character from the last group (if it exists) and concatenating all the groups in order, the resultant string should be s.
+//
+// // Given the string s, the size of each group k and the character fill, return a string array denoting the composition of every group s has been divided into, using the above procedure.
+
+// Example 1:
+
+// Input: s = "abcdefghi", k = 3, fill = "x"
+// Output: ["abc","def","ghi"]
+// Explanation:
+// // // // The first 3 characters "abc" form the first group.
+// // // The next 3 characters "def" form the second group.
+// // The last 3 characters "ghi" form the third group.
+// Since all groups can be completely filled by characters from the string, we do not need to use fill.
+// Thus, the groups formed are "abc", "def", and "ghi".
+// Example 2:
+
+// Input: s = "abcdefghij", k = 3, fill = "x"
+// Output: ["abc","def","ghi","jxx"]
+// Explanation:
+// Similar to the previous example, we are forming the first three groups "abc", "def", and "ghi".
+// For the last group, we can only use the character 'j' from the string. To complete this group, we add 'x' twice.
+// Thus, the 4 groups formed are "abc", "def", "ghi", and "jxx".
+
+function divideString(s, k, fill) {
+  let [newString, storeInArr, startCounter, endCounter] = ["", [], 0, 0];
+
+  for (let i = 0; i < s.length; i += k) {
+    newString = "";
+    endCounter += k;
+    newString += s.slice(startCounter, endCounter);
+    startCounter += k;
+    storeInArr.push(newString);
+  }
+  storeInArr.forEach((word, i) => {
+    if (word.length < k) {
+      const fillX = `${word}${fill.repeat(k - word.length)}`;
+      storeInArr[i] = fillX;
+    }
+  });
+  return storeInArr;
+}
+
+// console.log(divideString("abcdefghi", 3, "x"));
+// console.log(divideString("abcdefghij", 3, "x"));
+// console.log(divideString("ctoyjrwtngqwt", 8, "n"));
