95991edbaf6728a59e25c18c996b7c82dc405551
mohammad-anash <anash40678910@gmail.com>
2024-08-04 17:05:25 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -992,3 +992,94 @@ function maximumDiffrence(nums) {
 // console.log(maximumDiffrence([7, 1, 5, 4]));
 // console.log(maximumDiffrence([9, 4, 3, 2]));
 // console.log(maximumDiffrence([1, 5, 2, 10]));
+
+// 191 => merge Sorted Array
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Example 1:
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Example 2:
+//
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+// Explanation: The arrays we are merging are [1] and [].
+// The result of the merge is [1].
+// Example 3:
+
+// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+// Output: [1]
+// Explanation: The arrays we are merging are [] and [1].
+// The result of the merge is [1].
+// Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+function merge(nums1, m, nums2, n) {
+  for (let i = m; i < m + n; i++) {
+    nums1[i] = nums2[i - m];
+  }
+  return nums1;
+}
+
+// console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+// console.log(merge([1], 1, [], 0));
+// console.log(merge([0], 0, [1], 1));
+
+// 192 => Replace Elements with Greatest Element on Right Side
+
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+// Example 1:
+
+// Input: arr = [17,18,5,4,6,1]
+// Output: [18,6,6,6,1,-1]
+// Explanation:
+// - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+// - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+// - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+// - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+// - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+// - index 5 --> there are no elements to the right of index 5, so we put -1.
+// Example 2:
+
+// Input: arr = [400]
+// // Output: [-1]
+// Explanation: There are no elements to the right of index 0.
+
+function replaceElement(arr) {
+  let max = -1;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let current = arr[i];
+    arr[i] = max;
+    if (current > max) max = current;
+  }
+
+  // return arr;
+}
+
+// console.log(replaceElement([17, 18, 5, 4, 6, 1]));
+// console.log(replaceElement([400]));
+
+// Other Approch
+
+function replaceElement(nums) {
+  nums.reduceRight((max, current, index, arr) => {
+    const currentElement = current;
+    arr[index] = max;
+    return Math.max(max, currentElement);
+  }, -1);
+  return nums;
+}
+
+// console.log(replaceElement([17, 18, 5, 4, 6, 1]));
+// console.log(replaceElement([400]));
