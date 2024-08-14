95cb435bbd0fe84545a6ad148842d67bb8a8368c
mohammad-anash <anash40678910@gmail.com>
2024-08-15 01:16:16 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -3150,3 +3150,44 @@ function checkRecord(s) {
 // console.log(checkRecord("PPALLL"));
 // console.log(checkRecord("PPALLLA"));
 // console.log(checkRecord("ALLAPPL"));
+
+// 83 => Delete Characters to Make Fancy String
+
+// A fancy string is a string where no three consecutive characters are equal.
+// // Given a string s, delete the minimum possible number of characters from s to make it fancy.
+// Return the final string after the deletion. It can be shown that the answer will always be unique.
+
+// Example 1:
+
+// Input: s = "leeetcode"
+// Output: "leetcode"
+// Explanation:
+// Remove an 'e' from the first group of 'e's to create "leetcode".
+// No three consecutive characters are equal, so return "leetcode".
+// Example 2:
+
+// Input: s = "aaabaaaa"
+// Output: "aabaa"
+// Explanation:
+// // // Remove an 'a' from the first group of 'a's to create "aabaaaa".
+// // // Remove two 'a's from the second group of 'a's to create "aabaa".
+// // No three consecutive characters are equal, so return "aabaa".
+// Example 3:
+
+// Input: s = "aab"
+// Output: "aab"
+// Explanation: No three consecutive characters are equal, so return "aab".
+
+function makeFancyString(s) {
+  let newString = "";
+
+  for (let i = 0; i <= s.length - 1; i++) {
+    if (s[i] === s[i + 1] && s[i + 1] === s[i + 2]) newString += "";
+    else newString += s[i];
+  }
+
+  return newString;
+}
+
+console.log(makeFancyString("leeetcode"));
+console.log(makeFancyString("aaaabbbaaaaaaa"));
