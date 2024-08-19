b8b56c22c5a1509d67b5fb3d3978e9b298049970
mohammad-anash <anash40678910@gmail.com>
2024-08-19 13:30:32 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -1682,4 +1682,91 @@ function findMaxOnesRow(nums) {
 //     [0, 1, 1],
 //     [1, 1, 1],
 //   ])
+// )
+
+// 206 => Search a 2D Matrix - LeetCode
+
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+function searchMatrix(mat, tar) {
+  for (let i = 0; i < mat.length - 1; i++) {
+    for (let j = i; j < mat.length; j++) {
+      if (mat[i][j] === tar) return true;
+    }
+  }
+  return false;
+}
+
+// console.log(
+//   searchMatrix(
+//     [
+//       [1, 3, 5, 7],
+//       [10, 11, 16, 20],
+//       [23, 30, 34, 60],
+//     ],
+//     3
+//   )
+// );
+// console.log(
+//   searchMatrix(
+//     [
+//       [1, 3, 5, 7],
+//       [10, 11, 16, 20],
+//       [23, 30, 34, 60],
+//     ],
+//     13
+//   )
+// );
+
+// solve with binary search
+function searchMatrix(mat, tar) {
+  const oneDarr = mat.flat(Infinity);
+  let [low, high] = [0, oneDarr.length - 1];
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (oneDarr[mid] === tar) return true;
+    else if (oneDarr[mid] < tar) low = mid + 1;
+    else high = mid - 1;
+  }
+
+  return false;
+}
+
+// console.log(
+//   searchMatr
+//   ix(
+//     [
+//       [1, 3, 5, 7],
+//       [10, 11, 16, 20],
+//       [23, 30, 34, 60],
+//     ],
+//     3
+//   )
+// );
+// console.log(
+//   searchMatrix(
+//     [
+//       [1, 3, 5, 7],
+//       [10, 11, 16, 20],
+//       [23, 30, 34, 60],
+//     ],
+//     13
+//   )
 // );
