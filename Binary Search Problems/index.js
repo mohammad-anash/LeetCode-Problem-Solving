675a74071971ea5f5bf67a5499bfe28db831790a
mohammad-anash <anash40678910@gmail.com>
2024-08-21 22:12:28 +0530

    Updated Files

--- Binary Search Problems/index.js
+++ Binary Search Problems/index.js
@@ -95,3 +95,49 @@ function arranageCoins(n) {
 
 // console.log(arrangeCoins(5));
 // console.log(arrangeCoins(8));
+
+// 3 =>  Valid Perfect Square
+
+// Given a positive integer num, return true if num is a perfect square or false otherwise.
+// // A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+// You must not use any built-in library function, such as sqrt.
+
+// Example 1:
+
+// Input: num = 16
+// Output: true
+// // Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+// Example 2:
+
+// Input: num = 14
+// Output: false
+// Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+
+function isPerfectSquare(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i * i === n) return true;
+  }
+  return false;
+}
+
+// console.log(isPerfectSquare(16));
+// console.log(isPerfectSquare(14));
+
+// Solve With Binary Search
+
+function isPerfectSquare(n) {
+  let [low, high] = [1, n];
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let square = mid * mid;
+
+    if (square === n) return true;
+    else if (square > n) high = mid - 1;
+    else low = mid + 1;
+  }
+  return false;
+}
+
+console.log(isPerfectSquare(16));
+console.log(isPerfectSquare(14));
