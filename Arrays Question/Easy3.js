0fdaff7a5ca7a09925730902469064789af23777
mohammad-anash <anash40678910@gmail.com>
2024-09-07 23:54:38 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2355,3 +2355,44 @@ function findPeakElement(nums) {
 
 // console.log(findPeakElement([1, 2, 3, 1]));
 // console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4]))
+
+// 219 => Minimum Size Subarray Sum
+
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a
+// subarray
+//  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// // Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+function minSubArrayLen(target, nums) {
+  let compareVal = Infinity;
+  let sum = 0;
+  let left = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      compareVal = Math.min(compareVal, right - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+  }
+
+  return compareVal === Infinity ? 0 : compareVal;
+}
+
+// console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])); // Output: 2
+// console.log(minSubArrayLen(4, [1, 4, 4])); // Output: 1
+// console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])); // Output: 0
