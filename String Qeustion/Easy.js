ff97e6a5522f3b08ea4463ab202500d547555744
mohammad-anash <anash40678910@gmail.com>
2024-09-15 13:01:40 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -3409,3 +3409,51 @@ function reverseVowelString(s) {
 }
 
 // console.log(reverseVowelString("IceCreAm")); // Output: "ecICreAm"
+
+// 90 => Maximum Number of Operations With the Same Score I
+
+// You are given an array of integers nums. Consider the following operation:
+
+// Delete the first two elements nums and define the score of the operation as the sum of these two elements.
+// You can perform this operation until nums contains fewer than two elements. Additionally, the same score must be achieved in all operations.
+
+// Return the maximum number of operations you can perform.
+
+// Example 1:
+// Input: nums = [3,2,1,4,5]
+// Output: 2
+// Explanation:
+
+// We can perform the first operation with the score 3 + 2 = 5. After this operation, nums = [1,4,5].
+// We can perform the second operation as its score is 4 + 1 = 5, the same as the previous operation. After this operation, nums = [5].
+// As there are fewer than two elements, we can't perform more operations.
+// Example 2:
+
+// Input: nums = [1,5,3,3,4,1,3,2,2,3]
+// Output: 2
+// Explanation:
+
+// We can perform the first operation with the score 1 + 5 = 6. After this operation, nums = [3,3,4,1,3,2,2,3].
+// We can perform the second operation as its score is 3 + 3 = 6, the same as the previous operation. After this operation, nums = [4,1,3,2,2,3].
+// We cannot perform the next operation as its score is 4 + 1 = 5, which is different from the previous scores.
+// Example 3:
+
+// Input: nums = [5,3]
+// Output: 1
+
+function maxOperation(nums) {
+  if (nums.length < 2) return 0;
+  let sum = nums[0] + nums[1];
+  let counter = 1;
+
+  for (let i = 2; i < nums.length - 1; i += 2) {
+    if (nums[i] + nums[i + 1] === sum) counter++;
+    else break;
+  }
+  return counter;
+}
+
+console.log(maxOperation([3, 2, 1, 4, 5]));
+console.log(maxOperation([1, 5, 3, 3, 4, 1, 3, 2, 2, 3]));
+console.log(maxOperation([5, 3]));
+console.log(maxOperation([2, 2, 3, 2, 4, 2, 3, 3, 1, 3]));
