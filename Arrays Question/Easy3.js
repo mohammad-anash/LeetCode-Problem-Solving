aec4e6752a0053931378d482bfc5aedd77b3a1ef
mohammad-anash <anash40678910@gmail.com>
2024-10-01 11:16:51 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2679,3 +2679,42 @@ function nextGreatorElement(num1, num2) {
 
 // console.log(nextGreatorElement([4, 1, 2], [1, 3, 4, 2]));
 // console.log(nextGreatorElement([2, 4], [1, 2, 3]));
+
+// 227 => Maximum Average Subarray I
+
+// You are given an integer array nums consisting of n elements, and an integer k.
+
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+// Example 1:
+
+// Input: nums = [1,12,-5,-6,50,3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+// Example 2:
+
+// Input: nums = [5], k = 1
+// Output: 5.00000
+
+function findMaxAverage(nums, k) {
+  let windowSum = 0;
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i];
+  }
+
+  let maxSum = windowSum;
+
+  for (let i = k; i < nums.length; i++) {
+    windowSum = windowSum - nums[i - k] + nums[i];
+
+    maxSum = Math.max(maxSum, windowSum);
+  }
+
+  return maxSum / k;
+}
+
+// Test cases
+console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4)); // Output: 12.75
+console.log(findMaxAverage([5], 1)); // Output: 5.0
+console.log(findMaxAverage([0, 1, 1, 3, 3], 4)); // Output: 2.0
+console.log(findMaxAverage([0, 4, 0, 3, 2], 1)); // Output: 4.0
