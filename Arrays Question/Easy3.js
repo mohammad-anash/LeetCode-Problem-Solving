e2254f1342c2224eb8a4ab61772e26f1863bc3d3
mohammad-anash <anash40678910@gmail.com>
2024-10-02 11:11:54 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2714,7 +2714,40 @@ function findMaxAverage(nums, k) {
 }
 
 // Test cases
-console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4)); // Output: 12.75
-console.log(findMaxAverage([5], 1)); // Output: 5.0
-console.log(findMaxAverage([0, 1, 1, 3, 3], 4)); // Output: 2.0
-console.log(findMaxAverage([0, 4, 0, 3, 2], 1)); // Output: 4.0
+// console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4)); // Output: 12.75
+// console.log(findMaxAverage([5], 1)); // Output: 5.0
+// console.log(findMaxAverage([0, 1, 1, 3, 3], 4)); // Output: 2.0
+// console.log(findMaxAverage([0, 4, 0, 3, 2], 1)); // Output: 4.0
+
+// 228 =>Relative Sort Array
+
+// Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+// Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+
+// Example 1:
+
+// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+// Output: [2,2,2,1,4,3,3,9,6,7,19]
+// Example 2:
+
+// Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+// Output: [22,28,8,6,17,44]
+
+function relativeSortArray(nums1, nums2) {
+  const ans = [];
+  const filterRestValue = nums1.filter((val) => !nums2.includes(val));
+
+  for (const digit of nums2) {
+    const filterValue = nums1.filter((val) => digit === val);
+    ans.push(filterValue);
+  }
+  const inOneDArr = ans.flat(Infinity);
+  return [...inOneDArr, ...filterRestValue.sort((a, b) => a - b)];
+}
+
+console.log(
+  relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6])
+);
+
+console.log(relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]));
