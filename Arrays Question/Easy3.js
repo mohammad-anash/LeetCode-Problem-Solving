49a8cdcf7da08d212bab8aba435a078370cb28c6
mohammad-anash <anash40678910@gmail.com>
2024-10-02 12:04:03 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2746,8 +2746,58 @@ function relativeSortArray(nums1, nums2) {
   return [...inOneDArr, ...filterRestValue.sort((a, b) => a - b)];
 }
 
+// console.log(
+//   relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6])
+// );
+
+// console.log(relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]));
+
+// 229 =>Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+
+// Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+
+// Example 1:
+
+// Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+// Output: 1
+// Example 2:
+
+// Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+// Output: 3
+
+function numEquivDominoPair(dominoes) {
+  const map = {};
+  let counter = 0;
+
+  for (let [a, b] of dominoes) {
+    const key = a < b ? `${a}${b}` : `${b}${a}`;
+
+    if (map[key]) {
+      counter += map[key];
+      map[key]++;
+    } else {
+      map[key] = 1;
+    }
+  }
+
+  return counter;
+}
+
 console.log(
-  relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6])
+  numEquivDominoPair([
+    [1, 2],
+    [2, 1],
+    [3, 4],
+    [5, 6],
+  ])
 );
 
-console.log(relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]));
+console.log(
+  numEquivDominoPair([
+    [1, 2],
+    [1, 2],
+    [1, 1],
+    [1, 2],
+    [2, 2],
+  ])
+);
