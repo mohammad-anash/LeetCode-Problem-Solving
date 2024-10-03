b3dfaed2dae1ebedc64f7b18f9ee5bb6e9e5ada5
mohammad-anash <anash40678910@gmail.com>
2024-10-03 12:15:52 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2864,5 +2864,69 @@ const brackets = [
   [7, 10],
   [12, 25],
 ];
-const income = 10;
-console.log(calculateTax(brackets, income)); // Output: 2.65
+
+// const income = 10;
+// console.log(calculateTax(brackets, income)); // Output: 2.65
+
+// 231 => Cells with Odd Values in a Matrix
+
+// There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
+
+// For each location indices[i], do both of the following:
+
+// Increment all the cells on row ri.
+// Increment all the cells on column ci.
+// Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+//
+
+// Example 1:
+
+// Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+// Output: 6
+// Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+// After applying first increment it becomes [[1,2,1],[0,1,0]].
+// The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
+// Example 2:
+
+// Input: m = 2, n = 2, indices = [[1,1],[0,0]]
+// Output: 0
+// Explanation: Final matrix = [[2,2],[2,2]]. There are no odd numbers in the final matrix.
+
+function oddCells(m, n, indices) {
+  const arr = [];
+
+  for (let i = 0; i < m; i++) {
+    arr.push(new Array(n).fill(0));
+  }
+
+  for (let i = 0; i < indices.length; i++) {
+    let row = indices[i][0];
+    let col = indices[i][1];
+
+    for (let j = 0; j < n; j++) {
+      arr[row][j] += 1;
+    }
+
+    for (let j = 0; j < m; j++) {
+      arr[j][col] += 1;
+    }
+  }
+
+  let oddCount = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i][j] % 2 !== 0) {
+        oddCount++;
+      }
+    }
+  }
+
+  return oddCount;
+}
+
+// console.log(
+//   oddCells(2, 3, [
+//     [0, 1],
+//     [1, 1],
+//   ])
+// );
