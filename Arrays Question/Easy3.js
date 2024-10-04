822db12680a0de7e921ad5832e8dce71d01f3849
mohammad-anash <anash40678910@gmail.com>
2024-10-04 11:31:45 +0530

    Updated Files

--- Arrays Question/Easy3.js
+++ Arrays Question/Easy3.js
@@ -2930,3 +2930,85 @@ function oddCells(m, n, indices) {
 //     [1, 1],
 //   ])
 // );
+
+// 232 => 2404. Most Frequent Even Element
+
+// Given an integer array nums, return the most frequent even element.
+// If there is a tie, return the smallest one. If there is no such element, return -1.
+
+// Example 1:
+
+// Input: nums = [0,1,2,2,4,4,1]
+// Output: 2
+// Explanation:
+// // The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+// // We return the smallest one, which is 2.
+// Example 2:
+
+// Input: nums = [4,4,4,9,2,4]
+// Output: 4
+// Explanation: 4 is the even element appears the most.
+// Example 3:
+
+// Input: nums = [29,47,21,41,13,37,25,7]
+// Output: -1
+// Explanation: There is no even element.
+
+function mostFrequentEven(nums) {
+  const evenNums = [];
+  let obj = {};
+  let compare = -Infinity;
+  let update = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) evenNums.push(nums[i]);
+  }
+
+  if (evenNums.length === 0) {
+    return -1;
+  }
+
+  evenNums.forEach((curr) => {
+    if (obj[curr]) obj[curr]++;
+    else obj[curr] = 1;
+  });
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (value > compare) {
+      compare = value;
+      update = key;
+    }
+  }
+  return update;
+}
+
+// console.log(mostFrequentEven([4, 4, 4, 9, 2, 4]));
+// console.log(mostFrequentEven([29, 47, 21, 41, 13, 37, 25, 7]));
+// console.log(mostFrequentEven([0, 1, 2, 2, 4, 4, 1]));
+// console.log(mostFrequentEven([0, 1, 2, 0, 0, 0, 2, 4, 4, 1]));
+
+// Other Appproch
+
+function mostFrequentEven(nums) {
+  let compare = -Infinity;
+  let obj = {};
+  let updateVal;
+
+  nums.forEach((ele) => {
+    if (obj[ele]) obj[ele]++;
+    else obj[ele] = 1;
+  });
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (key % 2 === 0 && value > compare) {
+      compare = value;
+      updateVal = +key;
+    }
+  }
+  return updateVal ?? -1;
+}
+
+console.log(mostFrequentEven([4, 4, 4, 9, 2, 4]));
+console.log(mostFrequentEven([29, 47, 21, 41, 13, 37, 25, 7]));
+console.log(mostFrequentEven([0, 1, 2, 2, 4, 4, 1]));
+console.log(mostFrequentEven([0, 1, 2, 0, 0, 0, 2, 4, 4, 1]));
