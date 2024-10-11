0bdc3fb27d5aad02a96036248dec6672920e3b91
mohammad-anash <anash40678910@gmail.com>
2024-10-11 11:13:22 +0530

    updated File

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -412,3 +412,40 @@ function maxSubsequence(nums, k) {
 // console.log(maxSubsequence([2, 1, 3, 3], 2))
 // console.log(maxSubsequence([-1, -2, 3, 4], 3));
 // console.log(maxSubsequence([3, 4, 3, 3], 2));
+
+// 245 => Find Indices of Stable Mountains
+
+// There are n mountains in a row, and each mountain has a height. You are given an integer array height where height[i] represents the height of mountain i, and an integer threshold.
+
+// A mountain is called stable if the mountain just before it (if it exists) has a height strictly greater than threshold. Note that mountain 0 is not stable.
+
+// Return an array containing the indices of all stable mountains in any order.
+
+// Example 1:
+// Input: height = [1,2,3,4,5], threshold = 2
+// Output: [3,4]
+// Explanation:
+
+// Mountain 3 is stable because height[2] == 3 is greater than threshold == 2.
+// Mountain 4 is stable because height[3] == 4 is greater than threshold == 2.
+// Example 2:
+
+// Input: height = [10,1,10,1,10], threshold = 3
+// Output: [1,3]
+// Example 3:
+// Input: height = [10,1,10,1,10], threshold = 10
+// Output: []
+
+function stableMountain(nums, thresold) {
+  const ans = [];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] > thresold) {
+      ans.push(i);
+    }
+  }
+  return ans;
+}
+
+console.log(stableMountain([1, 2, 3, 4, 5], 2));
+console.log(stableMountain([10, 1, 10, 1, 10], 3));
