6059d75909b1b5c22c2355b5cefe11b38309ff06
mohammad-anash <anash40678910@gmail.com>
2024-10-12 22:16:03 +0530

    UPdated File

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -447,5 +447,48 @@ function stableMountain(nums, thresold) {
   return ans;
 }
 
-console.log(stableMountain([1, 2, 3, 4, 5], 2));
-console.log(stableMountain([10, 1, 10, 1, 10], 3));
+// console.log(stableMountain([1, 2, 3, 4, 5], 2));
+// console.log(stableMountain([10, 1, 10, 1, 10], 3));
+
+// 246 => You are given an array of positive integers nums.
+
+// Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+// Return true if Alice can win this game, otherwise, return false.
+
+// Example 1:
+// // Input: nums = [1,2,3,4,10]
+// Output: false
+// Explanation:
+// Alice cannot win by choosing either single-digit or double-digit numbers.
+
+// Example 2:
+// // Input: nums = [1,2,3,4,5,14]
+// Output: true
+// Explanation:
+// Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+// Example 3:
+// // Input: nums = [5,5,5,25]
+// Output: true
+// Explanation:
+// Alice can win by choosing double-digit numbers which have a sum equal to 25.
+
+function canAliceWin(nums) {
+  let singleDigitSum = 0;
+  let doubleDigitSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= 9) {
+      singleDigitSum += nums[i];
+    } else {
+      doubleDigitSum += nums[i];
+    }
+  }
+  return singleDigitSum > doubleDigitSum || doubleDigitSum > singleDigitSum
+    ? true
+    : false;
+}
+console.log(canAliceWin([1, 2, 3, 4, 10]));
+console.log(canAliceWin([1, 2, 3, 4, 5, 10]));
+console.log(canAliceWin([5, 5, 5, 25]));
