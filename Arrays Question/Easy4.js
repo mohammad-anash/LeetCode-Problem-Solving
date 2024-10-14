f430b20318dfb35a9b7743f63ea113aeab3cc4f6
mohammad-anash <anash40678910@gmail.com>
2024-10-14 22:48:24 +0530

    Updated File

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -531,7 +531,53 @@ function leftRightDifference(nums) {
     rightSum = nums.slice(i + 1).reduce((sum, current) => sum + current, 0);
     result.push(Math.abs(leftSum - rightSum));
   }
-  return result;
+  // return result;
 }
 
-console.log(leftRightDifference([10, 4, 8, 3]));
+// console.log(leftRightDifference([10, 4, 8, 3]));
+
+// 248 => You are given an integer array nums.
+
+// You replace each element in nums with the sum of its digits.
+
+// Return the minimum element in nums after all replacements.
+
+// Example 1:
+// // Input: nums = [10,12,13,14]
+// Output: 1
+// // Explanation:
+// nums becomes [1, 3, 4, 5] after all replacements, with minimum element 1.
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: 1
+// Explanation:
+// nums becomes [1, 2, 3, 4] after all replacements, with minimum element 1.
+
+// Example 3:
+// Input: nums = [999,19,199]
+// Output: 10
+// Explanation:
+// nums becomes [27, 10, 19] after all replacements, with minimum element 10.
+
+function minElement(nums) {
+  const values = [];
+
+  for (const num of nums) {
+    let sum = 0;
+    num
+      .toString()
+      .split('')
+      .forEach((val) => {
+        sum += +val;
+      });
+
+    values.push(sum);
+  }
+  return Math.min(...values);
+}
+
+// console.log(minElement([10, 12, 13, 14]));
+// console.log(minElement([1, 2, 3, 4]));
+// console.log(minElement([999, 19, 99]));
+// console.log(minElement([97]));
