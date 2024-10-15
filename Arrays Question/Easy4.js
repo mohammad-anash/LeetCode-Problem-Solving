384b97a002f0f56dcc8c33c5d92dcc510464a277
mohammad-anash <anash40678910@gmail.com>
2024-10-15 14:29:24 +0530

    Updated File

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -581,3 +581,42 @@ function minElement(nums) {
 // console.log(minElement([1, 2, 3, 4]));
 // console.log(minElement([999, 19, 99]));
 // console.log(minElement([97]));
+
+// 249 => In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+
+// As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+
+// Example 1:
+// // Input: nums = [0,1,1,0]
+// Output: [0,1]
+// Explanation:
+// The numbers 0 and 1 each appear twice in the array.
+
+// Example 2:
+// // Input: nums = [0,3,2,1,3,2]
+// Output: [2,3]
+// Explanation:
+// The numbers 2 and 3 each appear twice in the array.
+
+// Example 3:
+// Input: nums = [7,1,5,4,3,4,6,0,9,5,8,2]
+// Output: [4,5]
+// Explanation:
+// The numbers 4 and 5 each appear twice in the array.
+
+function getSneakyNumbers(nums) {
+  nums = nums.sort((a, b) => a - b);
+  const ans = [];
+
+  for (let i = 0; i < nums.length - 1; i += 1) {
+    if (nums[i] === nums[i + 1]) {
+      ans.push(nums[i]);
+    }
+  }
+  return ans;
+}
+
+console.log(getSneakyNumbers([0, 1, 1, 0]));
+console.log(getSneakyNumbers([0, 3, 2, 1, 3, 2]));
+console.log(getSneakyNumbers([7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2]));
+console.log(getSneakyNumbers([2, 0, 1, 2, 1]));
