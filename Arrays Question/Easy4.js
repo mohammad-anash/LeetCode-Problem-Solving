9f2bb2a44a2c6a7cc164ca18b40c31c4f69c04d6
mohammad-anash <anash40678910@gmail.com>
2024-10-16 17:58:04 +0530

    Updated File

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -616,7 +616,64 @@ function getSneakyNumbers(nums) {
   return ans;
 }
 
-console.log(getSneakyNumbers([0, 1, 1, 0]));
-console.log(getSneakyNumbers([0, 3, 2, 1, 3, 2]));
-console.log(getSneakyNumbers([7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2]));
-console.log(getSneakyNumbers([2, 0, 1, 2, 1]));
+// console.log(getSneakyNumbers([0, 1, 1, 0]));
+// console.log(getSneakyNumbers([0, 3, 2, 1, 3, 2]));
+// console.log(getSneakyNumbers([7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2]));
+// console.log(getSneakyNumbers([2, 0, 1, 2, 1]));
+
+// 250 =>  Find the Maximum Divisibility Score
+
+// You are given two integer arrays nums and divisors.
+// The divisibility score of divisors[i] is the number of indices j such that nums[j] is divisible by divisors[i].
+// Return the integer divisors[i] with the maximum divisibility score. If multiple integers have the maximum score, return the smallest one.
+
+// Example 1:
+// // Input: nums = [2,9,15,50], divisors = [5,3,7,2]
+// Output: 2
+// // Explanation:
+// // The divisibility score of divisors[0] is 2 since nums[2] and nums[3] are divisible by 5.
+// The divisibility score of divisors[1] is 2 since nums[1] and nums[2] are divisible by 3.
+// // The divisibility score of divisors[2] is 0 since none of the numbers in nums is divisible by 7.
+// The divisibility score of divisors[3] is 2 since nums[0] and nums[3] are divisible by 2.
+// As divisors[0], divisors[1], and divisors[3] have the same divisibility score, we return the smaller one which is divisors[3].
+
+// Example 2:
+// // Input: nums = [4,7,9,3,9], divisors = [5,2,3]
+// Output: 3
+// // Explanation:
+// // The divisibility score of divisors[0] is 0 since none of numbers in nums is divisible by 5.
+// The divisibility score of divisors[1] is 1 since only nums[0] is divisible by 2.
+// The divisibility score of divisors[2] is 3 since nums[2], nums[3] and nums[4] are divisible by 3.
+
+// Example 3:
+// Input: nums = [20,14,21,10], divisors = [10,16,20]
+// Output: 10
+// Explanation:
+// The divisibility score of divisors[0] is 2 since nums[0] and nums[3] are divisible by 10.
+// The divisibility score of divisors[1] is 0 since none of the numbers in nums is divisible by 16.
+// The divisibility score of divisors[2] is 1 since nums[0] is divisible by 20.
+
+function maxDivScore(nums, divisors) {
+  let res = -Infinity;
+  let counter;
+  let updateRes;
+
+  for (let i = 0; i < divisors.length; i++) {
+    counter = 0;
+
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] % divisors[i] === 0) counter++;
+    }
+
+    if (counter > res || (counter === res && divisors[i] < updateRes)) {
+      res = counter;
+      updateRes = divisors[i];
+    }
+  }
+  return updateRes;
+}
+
+console.log(maxDivScore([2, 9, 15, 50], [5, 3, 7, 2]));
+console.log(maxDivScore([4, 7, 9, 3, 9], [5, 2, 3]));
+console.log(maxDivScore([10, 14, 21, 10], [10, 16, 20]));
+console.log(maxDivScore([73, 13, 20, 6], [56, 75, 83, 26, 24, 53, 56, 61]));
