7c5ce9218ea8befba432feb44790f2e7aeab8d8a
mohammad-anash <anash40678910@gmail.com>
2024-11-10 12:11:56 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4279,5 +4279,60 @@ function isBalanced(s) {
   return evenIndSum === oddIndSum;
 }
 
-console.log(isBalanced('1234'));
-console.log(isBalanced('24123'));
+// console.log(isBalanced('1234'));
+// console.log(isBalanced('24123'));
+
+// 109 => Minimum recolors to get k consecutive blac blocks
+
+// You are given a 0-indexed string blocks of length n, where blocks[i] is either 'W' or 'B', representing the color of the ith block. The characters 'W' and 'B' denote the colors white and black, respectively.
+
+// You are also given an integer k, which is the desired number of consecutive black blocks.
+
+// In one operation, you can recolor a white block such that it becomes a black block.
+
+// Return the minimum number of operations needed such that there is at least one occurrence of k consecutive black blocks.
+
+// Example 1:
+
+// Input: blocks = "WBBWWBBWBW", k = 7
+// Output: 3
+// Explanation:
+// // // // One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 4th blocks
+// // // so that blocks = "BBBBBBBWBW".
+// // It can be shown that there is no way to achieve 7 consecutive black blocks in less than 3 operations.
+// Therefore, we return 3.
+// Example 2:
+
+// Input: blocks = "WBWBBBW", k = 2
+// Output: 0
+// Explanation:
+// No changes need to be made, since 2 consecutive black blocks already exist.
+// Therefore, we return 0.
+
+function minimumRecolors(blocks, k) {
+  let updateBlockCounter = Number.MAX_SAFE_INTEGER;
+
+  const value = 'B'.repeat(k);
+
+  for (let i = 0; i < blocks.length; i++) {
+    const partOfArr = blocks.slice(i, k + i);
+
+    if (partOfArr.length === k && value === partOfArr) {
+      return 0;
+    }
+
+    if (partOfArr.length === k) {
+      const countWBlocks = partOfArr
+        .split('')
+        .filter((current) => current === 'W').length;
+
+      if (countWBlocks < updateBlockCounter) {
+        updateBlockCounter = countWBlocks;
+      }
+    }
+  }
+  return updateBlockCounter;
+}
+
+// console.log(minimumRecolors('WBBWWBBWBW', 7));
+// console.log(minimumRecolors('WBWBBBW', 2));
