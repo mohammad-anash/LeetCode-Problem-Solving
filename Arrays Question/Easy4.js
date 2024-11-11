8a52a8790e2484e73eafa86a41a90c8c655261e1
mohammad-anash <anash40678910@gmail.com>
2024-11-11 12:05:04 +0530

    Updated Files

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -1350,8 +1350,47 @@ const merge = (arr) => {
 // console.log(
 //   merge([
 //     [1, 3],
-//     [2, 6],
+//     [2, 6]
 //     [8, 10],
 //     [15, 18],
 //   ])
 // ); // Output: [[1, 6], [8, 10], [15, 18]]
+
+// 269 => Group Anagrams
+
+// Given an array of strings strs, group the
+// anagrams
+//  together. You can return the answer in any order.
+
+// Example 1:
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Explanation:
+
+// There is no string in strs that can be rearranged to form "bat".
+// The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+// The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+// Example 2:
+// Input: strs = [""]
+// Output: [[""]]
+
+// Example 3:
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+function groupAnagrams(strs) {
+  const map = new Map();
+
+  for (let str of strs) {
+    const sortedStr = str.split('').sort().join('');
+
+    if (!map.has(sortedStr)) {
+      map.set(sortedStr, [str]);
+    } else {
+      map.get(sortedStr).push(str);
+    }
+  }
+  return Array.from(map.values());
+}
+
+// console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
