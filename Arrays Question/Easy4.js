eba3d53a29f7ce6e77720dad4205054a958bdefa
mohammad-anash <anash40678910@gmail.com>
2024-11-13 17:34:07 +0530

    Updated Files

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -1471,6 +1471,35 @@ function topKFrequent(nums, k) {
   return ans;
 }
 
-console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
-console.log(topKFrequent([3, 0, 1, 0], 1));
-console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2));
+// console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
+
+// 269 => Maximum Gap
+
+// Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+// You must write an algorithm that runs in linear time and uses linear extra space.
+
+// Example 1:
+
+// Input: nums = [3,6,9,1]
+// Output: 3
+// Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+// Example 2:
+
+// Input: nums = [10]
+// Output: 0
+// Explanation: The array contains less than 2 elements, therefore return 0.
+
+function maximumGap(nums) {
+  if (nums.length < 2) return 0;
+  let maxDifference = -Infinity;
+  nums = nums.sort((a, b) => a - b);
+
+  for (let i = 1; i < nums.length; i++) {
+    maxDifference = Math.max(maxDifference, nums[i] - nums[i - 1]);
+  }
+  return maxDifference;
+}
+
+// console.log(maximumGap([3, 6, 9, 1]));
+// console.log(maximumGap([10]));
