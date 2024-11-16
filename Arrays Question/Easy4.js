4be4e0fd72b729e999faecb7bc3dbf725fa21f20
mohammad-anash <anash40678910@gmail.com>
2024-11-16 13:14:10 +0530

    Updated Files

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -1597,4 +1597,39 @@ function findDuplicates(nums) {
 
 // console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]));
 
-console.log('hello');
+// 272 => can Place Flower
+
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+function canPlaceFlower(nums, n) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      nums[i] === 0 &&
+      (i === 0 || nums[i - 1] === 0) &&
+      (i === nums.length - 1 || nums[i + 1] === 0)
+    ) {
+      count++;
+      nums[i] = 1;
+    }
+  }
+
+  return count >= n;
+}
+
+console.log(canPlaceFlower([1, 0, 0, 0, 1], 1));
+console.log(canPlaceFlower([1, 0, 0, 0, 1], 2));
+console.log(canPlaceFlower([1, 0, 0, 0, 0, 1], 2));
+console.log(canPlaceFlower([0, 0, 1, 0, 1], 1));
