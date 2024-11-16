c32a4f04a1382da64d897352a8aea71d7adba482
mohammad-anash <anash40678910@gmail.com>
2024-11-16 15:43:48 +0530

    Updated Files

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -1629,7 +1629,33 @@ function canPlaceFlower(nums, n) {
   return count >= n;
 }
 
-console.log(canPlaceFlower([1, 0, 0, 0, 1], 1));
-console.log(canPlaceFlower([1, 0, 0, 0, 1], 2));
-console.log(canPlaceFlower([1, 0, 0, 0, 0, 1], 2));
-console.log(canPlaceFlower([0, 0, 1, 0, 1], 1));
+// console.log(canPlaceFlower([1, 0, 0, 0, 1], 1));
+
+// 273 => Rotate Array
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// // // rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// // rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation:
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+function rotate(nums, k) {
+  k = k % nums.length;
+  const lastEl = nums.splice(-k);
+  nums.unshift(...lastEl);
+  return nums;
+}
+
+// console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
