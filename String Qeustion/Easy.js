76736658bbc0fbe3ada9c34d3c102265768f07fd
mohammad-anash <anash40678910@gmail.com>
2024-11-17 20:31:58 +0530

    Updated File

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4480,3 +4480,53 @@ function maximumNumberOfBallon(s) {
 }
 
 // console.log(maximumNumberOfBallon('nlaebolko'));
+
+// 113 => Find the K-Beauty of a Number
+
+// The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+
+// It has a length of k.
+// It is a divisor of num.
+// Given integers num and k, return the k-beauty of num.
+
+// Note:
+
+// Leading zeros are allowed.
+// 0 is not a divisor of any value.
+// A substring is a contiguous sequence of characters in a string.
+
+// Example 1:
+
+// Input: num = 240, k = 2
+// Output: 2
+// Explanation: The following are the substrings of num of length k:
+// - "24" from "240": 24 is a divisor of 240.
+// - "40" from "240": 40 is a divisor of 240.
+// Therefore, the k-beauty is 2.
+// Example 2:
+
+// Input: num = 430043, k = 2
+// Output: 2
+// Explanation: The following are the substrings of num of length k:
+// - "43" from "430043": 43 is a divisor of 430043.
+// - "30" from "430043": 30 is not a divisor of 430043.
+// - "00" from "430043": 0 is not a divisor of 430043.
+// - "04" from "430043": 4 is not a divisor of 430043.
+// - "43" from "430043": 43 is a divisor of 430043.
+// Therefore, the k-beauty is 2.
+function divisorSubString(str, k) {
+  const inStr = str.toString();
+  let count = 0;
+
+  for (let i = 0; i < inStr.length; i++) {
+    const divisor = inStr.slice(i, i + k);
+    if (inStr % parseInt(divisor) === 0 && divisor.length === k) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// console.log(divisorSubString(240, 2));
+// console.log(divisorSubString(430043, 2));
+// console.log(divisorSubString(30003, 3));
