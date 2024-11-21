323fac3d6177f9f3aca1c1754e5fa464e908517e
mohammad-anash <anash40678910@gmail.com>
2024-11-21 11:17:15 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4703,7 +4703,29 @@ function longestConsecutive(nums) {
   return maxLength;
 }
 
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output: 4
-console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])); // Output: 9
-console.log(longestConsecutive([])); // Output: 0
-console.log(longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6])); // Output: 7
+// console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // Output:
+
+// 118 => Kth Largest Element in an Array
+
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+
+function findKthLargest(nums, k) {
+  const sortedArr = nums.sort((a, b) => a - b);
+  return sortedArr[sortedArr.length - k];
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
