8c4346d33eee5ca1a9a3427c28c09bf6a88a190f
mohammad-anash <anash40678910@gmail.com>
2024-11-22 17:43:07 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4748,6 +4748,7 @@ function findKthLargest(nums, k) {
 
 // Input: nums = [1]
 // Output: 0
+
 function numberOfArithmeticSlices(nums) {
   let counter = 0;
   if (nums.length < 3) return 0;
@@ -4777,3 +4778,59 @@ function numberOfArithmeticSlices(nums) {
 // console.log(numberOfArithmeticSlices([1, 2, 3, 4])); // Output: 3
 // console.log(numberOfArithmeticSlices([1])); // Output: 0
 // console.log(numberOfArithmeticSlices([1, 2, 3])); // Output: 1
+
+// 120 => Shortest Completing Word
+
+// Given a string licensePlate and an array of strings words, find the shortest completing word in words.
+
+// A completing word is a word that contains all the letters in licensePlate. Ignore numbers and spaces in licensePlate, and treat letters as case insensitive. If a letter appears more than once in licensePlate, then it must appear in the word the same number of times or more.
+
+// For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case), and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+
+// Return the shortest completing word in words. It is guaranteed an answer exists. If there are multiple shortest completing words, return the first one that occurs in words.
+
+// Example 1:
+
+// Input: licensePlate = "1s3 PSt", words = ["step","steps","stripe","stepple"]
+// Output: "steps"
+// Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and 't'.
+// "step" contains 't' and 'p', but only contains 1 's'.
+// "steps" contains 't', 'p', and both 's' characters.
+// "stripe" is missing an 's'.
+// "stepple" is missing an 's'.
+// Since "steps" is the only word containing all the letters, that is the answer.
+// Example 2:
+
+// Input: licensePlate = "1s3 456", words = ["looks","pest","stew","show"]
+// Output: "pest"
+// Explanation: licensePlate only contains the letter 's'. All the words contain 's', but among these "pest", "stew", and "show" are shortest. The answer is "pest" because it is the word that appears earliest of the 3.
+
+const shortestCompletingWords = (licensePlate, words) => {
+  let updateString = '';
+  licensePlate = licensePlate.toLowerCase();
+  let shortestWord = null;
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < licensePlate.length; j++) {
+      if (/[a-z]/.test(licensePlate[j])) {
+        let tempWord = words[i].split('');
+        let isCompleting = true;
+
+        const findInd = tempWord.indexOf(updateString[j]);
+
+        if (findInd !== -1) {
+          tempWord.splice(findInd, 1);
+        } else {
+          isCompleting = false;
+          break;
+        }
+      }
+    }
+  }
+
+  return shortestWord;
+};
+
+// console.log(
+//   shortestCompletingWords('1s3 PSt', ['step', 'steps', 'stripe', 'stepple'])
+// ); // Output: "steps"
