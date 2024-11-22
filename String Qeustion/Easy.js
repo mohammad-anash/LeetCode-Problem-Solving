0e4cad45d88cb4a33510986e34a2759ca3121d9f
mohammad-anash <anash40678910@gmail.com>
2024-11-22 12:11:17 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4727,5 +4727,53 @@ function findKthLargest(nums, k) {
   return sortedArr[sortedArr.length - k];
 }
 
-console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
-console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+// console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
+// console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+
+// 119 => Arithmetic Slices
+
+// An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+// For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+// Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+// A subarray is a contiguous subsequence of the array.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: 3
+// Explanation: We have 3 arithmetic slices in nums: [1, 2, 3], [2, 3, 4] and [1,2,3,4] itself.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 0
+function numberOfArithmeticSlices(nums) {
+  let counter = 0;
+  if (nums.length < 3) return 0;
+
+  function checkSameDifference(arr) {
+    const difference = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+      difference.push(arr[i + 1] - arr[i]);
+    }
+    return difference.every((val) => val === difference[0]);
+  }
+
+  for (let start = 0; start < nums.length - 2; start++) {
+    for (let end = start + 2; end < nums.length; end++) {
+      const subarray = nums.slice(start, end + 1);
+      if (checkSameDifference(subarray)) {
+        counter++;
+      } else {
+        break;
+      }
+    }
+  }
+
+  return counter;
+}
+
+// console.log(numberOfArithmeticSlices([1, 2, 3, 4])); // Output: 3
+// console.log(numberOfArithmeticSlices([1])); // Output: 0
+// console.log(numberOfArithmeticSlices([1, 2, 3])); // Output: 1
