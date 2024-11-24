af58f6e5ab9cd9fda12972a904f0316f081a8767
mohammad-anash <anash40678910@gmail.com>
2024-11-24 11:36:21 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4899,3 +4899,53 @@ const reformatPhoneNumber = (input) => {
 
 // Test cases
 // console.log(reformatPhoneNumber('1-23-45 6'));
+
+// 122 => Check if One String Swap Can Make Strings Equal
+
+// You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+// Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+
+// Example 1:
+
+// Input: s1 = "bank", s2 = "kanb"
+// Output: true
+// Explanation: For example, swap the first character with the last character of s2 to make "bank".
+// Example 2:
+
+// Input: s1 = "attack", s2 = "defend"
+// Output: false
+// // Explanation: It is impossible to make them equal with one string swap.
+// Example 3:
+
+// Input: s1 = "kelb", s2 = "kelb"
+// Output: true
+// Explanation: The two strings are already equal, so no string swap operation is required.
+
+const areAlmostEqual = (s1, s2) => {
+  if (s1 === s2) return true;
+
+  s1 = s1.split('');
+  s2 = s2.split('');
+  let counter = 0;
+  let mismatch = [];
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) {
+      mismatch.push(i);
+      counter++;
+      if (counter > 2) {
+        return false;
+      }
+    }
+  }
+
+  if (counter === 2) {
+    let [i, j] = mismatch;
+    return s1[i] === s2[j] && s1[j] === s2[i];
+  }
+
+  return false;
+};
+
+console.log(areAlmostEqual('kelb', 'kelb'));
