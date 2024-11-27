db2c1d6b31c4fc8a5468c8052aa320255151e25a
mohammad-anash <anash40678910@gmail.com>
2024-11-27 12:18:42 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -4996,32 +4996,52 @@ const areAlmostEqual = (s1, s2) => {
 
 // console.log(squareIsWhite('a1'));
 // console.log(squareIsWhite('h3'));
-// console.log(squareIsWhite('c7'));
+// console.log(squareIsWhite('c7'))
 
-const matrixSum = (nums) => {
-  const maxValues = [];
+// 124 => Modify the Matrix
 
-  for (let i = 0; i < nums.length; i++) {
-    let store = [];
-    nums[i].sort((a, b) => a - b);
+// Given a 0-indexed m x n integer matrix matrix, create a new 0-indexed matrix called answer. Make answer equal to matrix, then replace each element with the value -1 with the maximum element in its respective column.
+
+// Return the matrix answer.
+
+// Example 1:
+
+// Input: matrix = [[1,2,-1],[4,-1,6],[7,8,9]]
+// // Output: [[1,2,9],[4,8,6],[7,8,9]]
+// // // Explanation: The diagram above shows the elements that are changed (in blue).
+// // // - We replace the value in the cell [1][1] with the maximum value in the column 1, that is 8.
+// // - We replace the value in the cell [0][2] with the maximum value in the column 2, that is 9.
+// Example 2:
+
+// Input: matrix = [[3,-1],[5,2]]
+// Output: [[3,2],[5,2]]
+// Explanation: The diagram above shows the elements that are changed (in blue).
+
+const modifeidMatrix = (nums) => {
+  let answer = nums.map((row) => [...row]);
+
+  for (let i = 0; i < nums[0].length; i++) {
+    let maxInCol = -Infinity;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j][i] > maxInCol) {
+        maxInCol = nums[j][i];
+      }
+    }
 
     for (let j = 0; j < nums.length; j++) {
-      const findMax = Math.max(...nums[i]);
-      console.log(findMax);
+      if (answer[j][i] === -1) {
+        answer[j][i] = maxInCol;
+      }
     }
   }
 
-  return maxValues;
+  return answer;
 };
 
-console.log(
-  matrixSum([
-    [7, 2, 1],
-    [6, 4, 2],
-    [6, 5, 3],
-    [3, 2, 1],
-  ])
-);
-
-
-// https://leetcode.com/problems/sum-in-a-matrix/description/
+// console.log(
+//   modifeidMatrix([
+//     [1, 2, -1],
+//     [4, -1, 6],
+//     [7, 8, 9],
+//   ])
+// );
