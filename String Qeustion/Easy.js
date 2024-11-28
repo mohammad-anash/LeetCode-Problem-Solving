b5b9c686925299e92d2b6f1928beec8683ed2202
mohammad-anash <anash40678910@gmail.com>
2024-11-28 16:48:44 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -5045,3 +5045,56 @@ const modifeidMatrix = (nums) => {
 //     [7, 8, 9],
 //   ])
 // );
+
+// 125 => Maximum Number of Vowels in a Substring of Given Length
+
+// Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+// Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+// Example 1:
+
+// Input: s = "abciiidef", k = 3
+// Output: 3
+// Explanation: The substring "iii" contains 3 vowel letters.
+// Example 2:
+
+// Input: s = "aeiou", k = 2
+// Output: 2
+// // Explanation: Any substring of length 2 contains 2 vowels.
+// Example 3:
+
+// Input: s = "leetcode", k = 3
+// Output: 2
+// Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+const maxVowels = (s, k) => {
+  let updateVal = -Infinity;
+  let vowelCount = 0;
+  const vowels = new Set('aeiou');
+
+  for (let i = 0; i < k; i++) {
+    if (vowels.has(s[i])) {
+      vowelCount++;
+    }
+  }
+  updateVal = vowelCount;
+
+  for (let i = k; i < s.length; i++) {
+    if (vowels.has(s[i])) {
+      vowelCount++;
+    }
+
+    if (vowels.has(s[i - k])) {
+      vowelCount--;
+    }
+
+    updateVal = Math.max(updateVal, vowelCount);
+  }
+
+  return updateVal;
+};
+
+// console.log(maxVowels('abciiidef', 3));
+// console.log(maxVowels('aeiou', 2));
+// console.log(maxVowels('leetcode', 3));
