bf1d1796200d62ccf6a41be540e9843dfb38c78d
mohammad-anash <anash40678910@gmail.com>
2024-12-04 11:20:38 +0530

    Updated Files

--- Arrays Question/Easy4.js
+++ Arrays Question/Easy4.js
@@ -1833,3 +1833,57 @@ function kthSmallestPair(nums, k) {
 // console.log(kthSmallestPair([3, 1, 4, 1, 5], 2));
 // console.log(kthSmallestPair([1, 2, 3, 4, 5], 1));
 // console.log(kthSmallestPair([1, 3, 1, 5, 4], 0));
+
+// 278 => Degree of an array
+
+// Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+// Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+// Example 1:
+
+// Input: nums = [1,2,2,3,1]
+// Output: 2
+// Explanation:
+// The input array has a degree of 2 because both elements 1 and 2 appear twice.
+// Of the subarrays that have the same degree:
+// [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+// The shortest length is 2. So return 2.
+// Example 2:
+
+// Input: nums = [1,2,2,3,1,4,2]
+// Output: 6
+// Explanation:
+// The degree is 3 because the element 2 is repeated 3 times.
+// So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6
+
+const ShortestSubArr = (nums) => {
+  let updateVal = -Infinity;
+  let obj = {};
+
+  nums.forEach((ele) => {
+    if (obj[ele]) obj[ele]++;
+    else obj[ele] = 1;
+
+    if (obj[ele] > updateVal) {
+      updateVal = obj[ele];
+    }
+  });
+
+  let minLength = Infinity;
+
+  for (let key in obj) {
+    if (obj[key] === updateVal) {
+      const [firstInd, lastInd] = [
+        nums.indexOf(parseInt(key)),
+        nums.lastIndexOf(parseInt(key)),
+      ];
+      const length = lastInd - firstInd + 1;
+      minLength = Math.min(minLength, length);
+    }
+  }
+  return minLength;
+};
+
+// console.log(ShortestSubArr([1, 2, 2, 3, 1]));
+// console.log(ShortestSubArr([1, 2, 2, 3, 1, 4, 2]));
