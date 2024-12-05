2f4734bcf774151634331000ec7ada2ed60fe078
mohammad-anash <anash40678910@gmail.com>
2024-12-05 11:16:07 +0530

    Updated Files

--- String Qeustion/Easy.js
+++ String Qeustion/Easy.js
@@ -5145,3 +5145,48 @@ function findLHS(nums) {
 // console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7]));
 // console.log(findLHS([1, 2, 3, 4]));
 // console.log(findLHS([1, 1, 1, 1]));
+
+// 127 =>  Check if Two Chessboard Squares Have the Same Color
+
+// You are given two strings, coordinate1 and coordinate2, representing the coordinates of a square on an 8 x 8 chessboard.
+
+// Below is the chessboard for reference.
+
+// Return true if these two squares have the same color and false otherwise.
+
+// The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first (indicating its column), and the number second (indicating its row).
+
+// Example 1:
+// Input: coordinate1 = "a1", coordinate2 = "c3"
+// Output: true
+// Explanation:
+// Both squares are black.
+
+// Example 2:
+// Input: coordinate1 = "a1", coordinate2 = "h3"
+// Output: false
+// Explanation:
+// Square "a1" is black and "h3" is white.
+
+const checkTwoChessBoards = (coord1, coord2) => {
+  const obj = {
+    a: ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
+    b: ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black'],
+    c: ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
+    d: ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black'],
+    e: ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
+    f: ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black'],
+    g: ['black', 'white', 'black', 'white', 'black', 'white', 'black', 'white'],
+    h: ['white', 'black', 'white', 'black', 'white', 'black', 'white', 'black'],
+  };
+
+  const [firstLet, secondLet] = [coord1.charAt(0), coord2.charAt(0)];
+  const [firstPos, secondPos] = [coord1.charAt(1) - 1, coord2.charAt(1) - 1];
+
+  const firstCoordi = obj[firstLet][firstPos];
+  const secondCoordi = obj[secondLet][secondPos];
+
+  return firstCoordi === secondCoordi;
+};
+
+console.log(checkTwoChessBoards('h7', 'c8')); // Output: true
