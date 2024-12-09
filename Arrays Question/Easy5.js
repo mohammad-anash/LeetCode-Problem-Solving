b1fa7a49076208a3bcd21192afe0586b1f539ccb
mohammad-anash <anash40678910@gmail.com>
2024-12-09 13:52:34 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -119,6 +119,7 @@ function countSubarrays(arr, k, threshold) {
 // Output: 3
 // Explanation: The answer is "wke", with the length of 3.
 // Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
 const lengthOfLongestSubstring = (s) => {
   let [l, r, maxlength] = [0, 0, 0];
 
@@ -149,3 +150,43 @@ const lengthOfLongestSubstring = (s) => {
 // console.log(lengthOfLongestSubstring('abcabcbb')); // Output: 3
 // console.log(lengthOfLongestSubstring('bbbbb')); // Output: 1
 // console.log(lengthOfLongestSubstring('pwwkew')); // Output: 3
+
+// 282 => Max Consecutive Ones III
+
+// Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+// Example 1:
+
+// Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+// Output: 6
+// // Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+// // Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+// Example 2:
+
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+const longestOnes = (nums, k) => {
+  let [l, r, zeros, maxlen] = [0, 0, 0, 0];
+
+  while (r < nums.length) {
+    if (nums[r] === 0) {
+      zeros++;
+    }
+
+    if (zeros > k) {
+      if (nums[l] === 0) {
+        zeros--;
+      }
+      l++;
+    }
+
+    maxlen = Math.max(maxlen, r - l + 1);
+    r++;
+  }
+  return maxlen;
+};
+
+// console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2));
