5390f0ead77e16305a3b938d96ae9f7b97d37a43
mohammad-anash <anash40678910@gmail.com>
2024-12-11 12:29:04 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -227,5 +227,44 @@ const numberOfSubstring = (s) => {
   return maxlen;
 };
 
-console.log(numberOfSubstring('bcbbbcba')); // Output: 4
-console.log(numberOfSubstring('aaaa')); // Output: 2
+// console.log(numberOfSubstring('bcbbbcba')); // Output: 4
+// console.log(numberOfSubstring('aaaa')); // Output: 2
+
+// 284 => Happy Number
+
+// Write an algorithm to determine if a number n is happy.
+
+// A happy number is a number defined by the following process:
+
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+// Example 1:
+
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+
+// Input: n = 2
+// Output: false
+
+const isHappy = (n) => {
+  let sum = n;
+
+  while (sum !== 1 && sum !== 4) {
+    sum = sum
+      .toString()
+      .split('')
+      .reduce((acc, digit) => acc + digit * digit, 0);
+  }
+
+  return sum === 1;
+};
+
+// console.log(isHappy(19));
