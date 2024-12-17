8f508a1d7e77ce0ac010b497aadafdb7fb2755b1
mohammad-anash <anash40678910@gmail.com>
2024-12-17 13:05:26 +0530

    UpdateD fILES

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -683,3 +683,42 @@ const kthSmallestPrimeFraction = (arr, k) => {
 
 // console.log(kthSmallestPrimeFraction([1, 2, 3, 5], 3)); // Output: [2, 5]
 // console.log(kthSmallestPrimeFraction([1, 7], 1)); // Output: [2, 5]
+
+// 303 => Single Number III
+
+// Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+// You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+// Example 1:
+
+// Input: nums = [1,2,1,3,2,5]
+// Output: [3,5]
+// // Explanation:  [5, 3] is also a valid answer.
+// Example 2:
+
+// Input: nums = [-1,0]
+// Output: [-1,0]
+// Example 3:
+
+// Input: nums = [0,1]
+// Output: [1,0]
+
+const singleNumber = (nums) => {
+  const storeNums = [];
+  let obj = {};
+
+  nums.forEach((ele) => {
+    if (obj[ele]) obj[ele]++;
+    else obj[ele] = 1;
+  });
+
+  for (const keys in obj) {
+    if (obj[keys] === 1 && storeNums.length < 3) storeNums.push(+keys);
+  }
+
+  return storeNums;
+};
+
+// console.log(singleNumber([1, 2, 1, 3, 2, 5]));
+// console.log(singleNumber([1, 0]));
