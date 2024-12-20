875a0e071ca1d90c1eb22507e6fb180d5b30d02d
mohammad-anash <anash40678910@gmail.com>
2024-12-20 23:24:52 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -793,6 +793,28 @@ const judgeSquareSum = (c) => {
 // console.log(judgeSquareSum(3));
 // console.log(judgeSquareSum(4));
 
+// 306 =>  Max Number of K-Sum Pairs
+
+// You are given an integer array nums and an integer k.
+// // In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+// Return the maximum number of operations you can perform on the array.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4], k = 5
+// Output: 2
+// Explanation: Starting with nums = [1,2,3,4]:
+// - Remove numbers 1 and 4, then nums = [2,3]
+// - Remove numbers 2 and 3, then nums = []
+// There are no more pairs that sum up to 5, hence a total of 2 operations.
+// Example 2:
+
+// Input: nums = [3,1,3,4,3], k = 6
+// Output: 1
+// Explanation: Starting with nums = [3,1,3,4,3]:
+// - Remove the first two 3's, then nums = [1,4,3]
+// There are no more pairs that sum up to 6, hence a total of 1 operation.
+
 function maxOperation(nums, k) {
   let countOperation = 0;
   const map = new Map();
@@ -810,7 +832,30 @@ function maxOperation(nums, k) {
   return countOperation;
 }
 
-console.log(maxOperation([1, 2, 3, 4], 5));
-console.log(
-  maxOperation([4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4], 2)
-);
+// console.log(maxOperation([1, 2, 3, 4], 5));
+// console.log(
+//   maxOperation([4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4], 2)
+// );
+
+const topKFrequent = (nums, k) => {
+  let obj = {};
+
+  nums.forEach((ele) => {
+    if (obj[ele]) obj[ele]++;
+    else obj[ele] = 1;
+  });
+
+  const getValues = Object.keys(obj).sort((a, b) => b - a);
+
+  for (let i = 0; i < k; i++) {
+    console.log(obj[getValues]);
+  }
+};
+
+// console.log(topKFrequent(['i', 'love', 'leetcode', 'i', 'love', 'coding'], 2));
+// console.log(
+//   topKFrequent(
+//     ['the', 'day', 'is', 'sunny', 'the', 'the', 'the', 'sunny', 'is', 'is'],
+//     4
+//   )
+// );
