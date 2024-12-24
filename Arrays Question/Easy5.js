ead3798262b25093d4302ba17573981dbbfea6ab
mohammad-anash <anash40678910@gmail.com>
2024-12-24 11:45:59 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -1052,3 +1052,43 @@ const vowelString = (words, queries) => {
 //     ]
 //   )
 // );
+
+// 312 => Pairs of Songs With Total Durations Divisible by 60
+
+// You are given a list of songs where the ith song has a duration of time[i] seconds.
+
+// Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+
+// Example 1:
+
+// Input: time = [30,20,150,100,40]
+// Output: 3
+// Explanation: Three pairs have a total duration divisible by 60:
+// (time[0] = 30, time[2] = 150): total duration 180
+// (time[1] = 20, time[3] = 100): total duration 120
+// (time[1] = 20, time[4] = 40): total duration 60
+// Example 2:
+
+// Input: time = [60,60,60]
+// Output: 3
+// Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+
+const numsPairDivisibleBy60 = (nums) => {
+  const remainderFreq = Array(60).fill(0);
+  let count = 0;
+
+  for (let time of nums) {
+    const remainder = time % 60;
+    const complement = (60 - remainder) % 60;
+
+    count += remainderFreq[complement];
+
+    remainderFreq[remainder]++;
+  }
+
+  return count;
+};
+
+console.log(numsPairDivisibleBy60([30, 20, 150, 100, 40]));
+console.log(numsPairDivisibleBy60([60, 60, 60]));
+console.log(numsPairDivisibleBy60(new Array(1e6).fill(60)));
