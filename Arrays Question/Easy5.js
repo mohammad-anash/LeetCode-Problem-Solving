450a1d34c3da8ffa0baf188485c30dde5e591426
mohammad-anash <anash40678910@gmail.com>
2024-12-27 12:00:46 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -1143,4 +1143,57 @@ const findMaximumXOR = (nums) => {
   return finalSum;
 };
 
-console.log(findMaximumXOR([21, 4, 7])); // Output: 32
+// console.log(findMaximumXOR([21, 4, 7])); // Output: 32
+
+// 314 => Best SightSeeing pair
+
+// You are given an integer array values where values[i] represents the value of the ith sightseeing spot. Two sightseeing spots i and j have a distance j - i between them.
+
+// The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: the sum of the values of the sightseeing spots, minus the distance between them.
+
+// Return the maximum score of a pair of sightseeing spots.
+
+// Example 1:
+
+// Input: values = [8,1,5,2,6]
+// Output: 11
+// Explanation: i = 0, j = 2, values[i] + values[j] + i - j = 8 + 5 + 0 - 2 = 11
+// Example 2:
+
+// Input: values = [1,2]
+// Output: 2
+
+function maxScoreSightSeeingPair(nums) {
+  let maxScore = 0;
+  let maxLeftContribution = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    maxScore = Math.max(maxScore, maxLeftContribution + nums[i] - i);
+
+    maxLeftContribution = Math.max(maxLeftContribution, nums[i] + i);
+  }
+
+  return maxScore;
+}
+
+// console.log(maxScoreSightSeeingPair([8, 1, 5, 2, 6]));
+
+const numOffSubarrays = (nums) => {
+  let prefixSum = 0;
+  const storeSubArray = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    prefixSum = 0;
+    for (let j = i; j < nums.length; j++) {
+      prefixSum += nums[j];
+      if (prefixSum % 2 !== 0) {
+        storeSubArray.push(nums.slice(i, j + 1));
+      }
+    }
+  }
+  return storeSubArray;
+};
+
+// console.log(numOffSubarrays([1, 3, 5]));
+// console.log(numOffSubarrays([2, 4, 6]));
+// console.log(numOffSubarrays([1, 2, 3, 4, 5, 6, 7]));
