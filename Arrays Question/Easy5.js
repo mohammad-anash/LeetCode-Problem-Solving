3eec364236c93b7b7b120ec59152177a3026a303
mohammad-anash <anash40678910@gmail.com>
2024-12-27 23:17:55 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -1291,5 +1291,54 @@ const subsets = (nums) => {
   return result;
 };
 
-console.log(subsets([1, 2]));
-console.log(subsets([1, 2, 3]));
+// console.log(subsets([1, 2]));
+// console.log(subsets([1, 2, 3]));
+
+// 318 => The Number of Beautiful Subsets
+
+// You are given an array nums of positive integers and a positive integer k.
+// A subset of nums is beautiful if it does not contain two integers with an absolute difference equal to k.
+// Return the number of non-empty beautiful subsets of the array nums.
+// A subset of nums is an array that can be obtained by deleting some (possibly none) elements from nums. Two subsets are different if and only if the chosen indices to delete are different.
+
+// Example 1:
+
+// Input: nums = [2,4,6], k = 2
+// Output: 4
+// // Explanation: The beautiful subsets of the array nums are: [2], [4], [6], [2, 6].
+// // It can be proved that there are only 4 beautiful subsets in the array [2,4,6].
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: 1
+// Explanation: The beautiful subset of the array nums is [1].
+// It can be proved that there is only 1 beautiful subset in the array [1].
+
+const beautifulSubsets = (nums, k) => {
+  let countBeautifulSubsets = 0;
+  nums.sort((a, b) => a - b);
+
+  const isBeautifulSubset = (nums) => {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (Math.abs(nums[i] - nums[j]) === k) return false;
+      }
+    }
+    return true;
+  };
+
+  const backtrack = (start, subset) => {
+    if (isBeautifulSubset([...subset])) countBeautifulSubsets++;
+    for (let i = start; i < nums.length; i++) {
+      subset.push(nums[i]);
+      backtrack(i + 1, subset);
+      subset.pop();
+    }
+  };
+  backtrack(0, []);
+  return countBeautifulSubsets - 1;
+};
+
+// console.log(beautifulSubsets([2, 4, 6], 2));
+// console.log(beautifulSubsets([1], 1));
+// console.log(beautifulSubsets([10, 4, 5, 7, 2, 1], 3));
