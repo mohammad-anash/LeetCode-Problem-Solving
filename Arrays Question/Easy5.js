1460ce6010ea8566aeeaaf41b4b3a3261743ea06
mohammad-anash <anash40678910@gmail.com>
2024-12-29 11:15:11 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -1434,3 +1434,54 @@ const permute = (nums, currArr = [], res = []) => {
 };
 
 // console.log(permute([1, 2, 3]));
+
+// 321 => Permutations II
+
+// Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output:
+// [[1,1,2],
+//  [1,2,1],
+//  [2,1,1]]
+// Example 2:
+
+// Input: nums = [1, 2, 3];
+// Output: [
+//   [1, 2, 3],
+//   [1, 3, 2],
+//   [2, 1, 3],
+//   [2, 3, 1],
+//   [3, 1, 2],
+//   [3, 2, 1],
+// ];
+
+const permuteUnique = (nums) => {
+  const map = new Set();
+
+  const generateAllPermutation = (nums, currArr = [], res = []) => {
+    if (nums.length === 0) {
+      const key = currArr.join(',');
+      if (!map.has(key)) {
+        map.add(key);
+        res.push([...currArr]);
+      }
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      const rest = nums.filter((_, j) => i !== j);
+      currArr.push(nums[i]);
+      generateAllPermutation(rest, currArr, res);
+      currArr.pop();
+    }
+
+    return res;
+  };
+
+  return generateAllPermutation(nums);
+};
+
+// console.log(permuteUnique([1, 1, 2]));
