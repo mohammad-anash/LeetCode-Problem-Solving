eefe8970d0aca6d11c5cac2456717f3eca2f37a7
mohammad-anash <anash40678910@gmail.com>
2025-01-11 00:05:36 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2052,3 +2052,47 @@ const findLongestChain = (nums) => {
 //     [3, 4],
 //   ])
 // )
+
+// 334 => count the number of Fair Pair
+
+// Given a 0-indexed integer array nums of size n and two integers lower and upper, return the number of fair pairs.
+
+// A pair (i, j) is fair if:
+
+// 0 <= i < j < n, and
+// lower <= nums[i] + nums[j] <= upper
+
+// Example 1:
+
+// Input: nums = [0,1,7,4,4,5], lower = 3, upper = 6
+// Output: 6
+// // Explanation: There are 6 fair pairs: (0,3), (0,4), (0,5), (1,3), (1,4), and (1,5).
+// Example 2:
+
+// Input: nums = [1,7,9,2,5], lower = 11, upper = 11
+// Output: 1
+// Explanation: There is a single fair pair: (2,3).
+
+function countFairPairs(nums, lower, upper) {
+  nums.sort((a, b) => a - b);
+
+  function getPairLessThan(arr, value) {
+    let count = 0;
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+      if (nums[left] + nums[right] < value) {
+        count += right - left;
+        left++;
+      } else {
+        right--;
+      }
+    }
+    return count;
+  }
+  return getPairLessThan(nums, upper + 1) - getPairLessThan(nums, lower);
+}
+
+// console.log(countFairPairs([0, 1, 7, 4, 4, 5], 3, 6)); // Output: 6
+// console.log(countFairPairs([1, 7, 9, 2, 5], 11, 11)); // Output: 1
