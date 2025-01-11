490e380c5047bd18bc4ace3ad85f946cd5c15bf6
mohammad-anash <anash40678910@gmail.com>
2025-01-12 00:27:49 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2188,4 +2188,46 @@ const rangeSum = (nums, n, left, right) => {
   return result;
 };
 
-console.log(rangeSum([1, 2, 3, 4], 4, 1, 5));
+// console.log(rangeSum([1, 2, 3, 4], 4, 1, 5));
+
+// 337 => Non-decreasing Array
+
+// Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+// We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+// Example 1:
+
+// Input: nums = [4,2,3]
+// Output: true
+// // Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+// Example 2:
+
+// Input: nums = [4,2,1]
+// Output: false
+// Explanation: You
+
+const checkPossibility = (nums) => {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      count++;
+
+      if (count > 1) return false;
+
+      if (i > 0 && nums[i - 1] > nums[i + 1]) {
+        nums[i + 1] = nums[i];
+      } else {
+        nums[i] = nums[i + 1];
+      }
+    }
+  }
+
+  return true;
+};
+
+// Test cases
+console.log(checkPossibility([4, 2, 3])); // Output: true
+console.log(checkPossibility([4, 2, 1])); // Output: false
+console.log(checkPossibility([3, 4, 2, 3])); // Output: false
