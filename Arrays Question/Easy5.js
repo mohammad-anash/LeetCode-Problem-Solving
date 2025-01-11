e877644c524d1ee920ce2c89f03787cbe8873002
mohammad-anash <anash40678910@gmail.com>
2025-01-11 12:30:20 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2096,3 +2096,53 @@ function countFairPairs(nums, lower, upper) {
 
 // console.log(countFairPairs([0, 1, 7, 4, 4, 5], 3, 6)); // Output: 6
 // console.log(countFairPairs([1, 7, 9, 2, 5], 11, 11)); // Output: 1
+
+// 335 => Find the Minimum Number of Fibonacci Numbers Whose Sum Is K
+
+// Given an integer k, return the minimum number of Fibonacci numbers whose sum is equal to k. The same Fibonacci number can be used multiple times.
+
+// The Fibonacci numbers are defined as:
+
+// F1 = 1
+// F2 = 1
+// Fn = Fn-1 + Fn-2 for n > 2.
+// It is guaranteed that for the given constraints we can always find such Fibonacci numbers that sum up to k.
+
+// Example 1:
+
+// Input: k = 7
+// Output: 2
+// // Explanation: The Fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, ...
+// // For k = 7 we can use 2 + 5 = 7.
+// Example 2:
+
+// Input: k = 10
+// Output: 2
+// // Explanation: For k = 10 we can use 2 + 8 = 10.
+// Example 3:
+
+// Input: k = 19
+// Output: 3
+// Explanation: For k = 19 we can use 1 + 5 + 13 = 19
+
+const findMinFibonacciNumbers = (k) => {
+  const fib = [1, 1];
+
+  while (true) {
+    const next = fib[fib.length - 1] + fib[fib.length - 2];
+    if (next > k) break;
+    fib.push(next);
+  }
+
+  let count = 0;
+  for (let i = fib.length - 1; i >= 0 && k > 0; i--) {
+    if (fib[i] <= k) {
+      k -= fib[i];
+      count++;
+    }
+  }
+
+  return count;
+};
+
+// console.log(findMinFibonacciNumbers(7))
