fc25b871903c4168b3b8baa47d0f31b4217b36ec
mohammad-anash <anash40678910@gmail.com>
2025-01-16 12:35:29 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2405,5 +2405,38 @@ const subArrayWithTwoDistinct = (nums) => {
   return maxLength;
 };
 
-console.log(subArrayWithTwoDistinct([2, 1, 2]));
-console.log(subArrayWithTwoDistinct([3, 1, 2, 2, 2, 2]));
+// console.log(subArrayWithTwoDistinct([2, 1, 2]));
+// console.log(subArrayWithTwoDistinct([3, 1, 2, 2, 2, 2]))
+
+// 343 => Number of Common Factors
+
+// Given two positive integers a and b, return the number of common factors of a and b.
+
+// An integer x is a common factor of a and b if x divides both a and b.
+
+// Example 1:
+
+// Input: a = 12, b = 6
+// Output: 4
+// Explanation: The common factors of 12 and 6 are 1, 2, 3, 6.
+// Example 2:
+
+// Input: a = 25, b = 30
+// Output: 2
+// Explanation: The common factors of 25 and 30 are 1, 5.
+
+const commonFactors = (a, b) => {
+  const n = Math.max(a, b);
+  let countCommonFac = 0;
+
+  for (let i = 0; i <= n; i++) {
+    if (a % i === 0 && b % i === 0) {
+      countCommonFac++;
+    }
+  }
+  return countCommonFac;
+};
+
+// console.log(commonFactors(12, 6));
+// console.log(commonFactors(25, 30));
+// console.log(commonFactors(885, 885));
