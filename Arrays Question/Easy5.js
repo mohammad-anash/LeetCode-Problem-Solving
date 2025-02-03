4ef7e0cffa52f77a5bb38a56562303c5ea95e343
mohammad-anash <anash40678910@gmail.com>
2025-02-03 23:11:42 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2909,4 +2909,4 @@ function shiftingLetters(s, shifts) {
 }
 
 // console.log(shiftingLetters('abc', [3, 5, 9])); // Output: "rpl"
-// console.log(shiftingLetters('bad', [10, 20, 30])); // Output: "jyh"git 
+// console.log(shiftingLetters('bad', [10, 20, 30])); // Output: "jyh"git
