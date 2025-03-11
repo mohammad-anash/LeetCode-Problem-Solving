9754812a43ff9db20d425f5d4a1264ca4e0b1144
mohammad-anash <anash40678910@gmail.com>
2025-03-11 22:53:03 +0530

    Solved Some Problems

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -2989,3 +2989,33 @@ const countDigits = (n) => {
 // console.log(countDigits(121));
 // console.log(countDigits(1248));
 // console.log(countDigits(7));
+
+// 358 => Three Divisors
+
+// Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+// An integer m is a divisor of n if there exists an integer k such that n = k * m.
+
+// Example 1:
+
+// Input: n = 2
+// Output: false
+// // Explantion: 2 has only two divisors: 1 and 2.
+// Example 2:
+
+// Input: n = 4
+// Output: true
+// Explantion: 4 has three divisors: 1, 2, and 4
+
+const isThree = (n) => {
+  const storeDivisors = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      storeDivisors.push(i);
+    }
+  }
+  return storeDivisors.length === 3 ? true : false;
+};
+
+console.log(isThree(2));
+console.log(isThree(4));
