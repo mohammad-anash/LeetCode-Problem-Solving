3297e25e4a2daaa8c05786d10a8ca6e0ecb9aa51
mohammad-anash <anash40678910@gmail.com>
2025-03-23 23:12:26 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -3350,5 +3350,39 @@ const countParitions = (nums) => {
   }
   return countValidParitions;
 };
-console.log(countParitions([10, 10, 3, 7, 6]));
-console.log(countParitions([1, 2, 2]));
+
+// console.log(countParitions([10, 10, 3, 7, 6]));
+// console.log(countParitions([1, 2, 2]));
+
+// 367 =>  Maximum Difference Between Adjacent Elements in a Circular Array
+
+// Given a circular array nums, find the maximum absolute difference between adjacent elements.
+// Note: In a circular array, the first and last elements are adjacent.
+
+// Example 1:
+// Input: nums = [1,2,4]
+// Output: 3
+// Explanation:
+// Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+
+// Example 2:
+// // Input: nums = [-5,-10,-5]
+// Output: 5
+// // Explanation:
+// The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+
+const maxAdjacentDistance = (nums) => {
+  let difference = Math.abs(nums.at(-1) - nums[0]);
+
+  for (let i = 0; i < nums.length; i++) {
+    const pairDifference = Math.abs(nums[i] - nums[i + 1]);
+    if (pairDifference > difference) {
+      difference = pairDifference;
+    }
+  }
+  return difference;
+};
+
+// console.log(maxAdjacentDistance([1, 2, 4]));
+// console.log(maxAdjacentDistance([-5, -10, -5]));
+// console.log(maxAdjacentDistance([2, 1, 0]));
