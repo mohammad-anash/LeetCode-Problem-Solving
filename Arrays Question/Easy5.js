5b1160f3a3fd761625a9a7a76f61deba3b51b285
mohammad-anash <anash40678910@gmail.com>
2025-03-23 22:39:54 +0530

    Updated Files

--- Arrays Question/Easy5.js
+++ Arrays Question/Easy5.js
@@ -3300,3 +3300,55 @@ const transformArray = (nums) => {
 };
 
 // console.log(transformArray([4, 3, 2, 1]));
+
+// 366 =>  Count Partitions with Even Sum Difference
+
+// You are given an integer array nums of length n.
+
+// A partition is defined as an index i where 0 <= i < n - 1, splitting the array into two non-empty subarrays such that:
+
+// Left subarray contains indices [0, i].
+// Right subarray contains indices [i + 1, n - 1].
+// Return the number of partitions where the difference between the sum of the left and right subarrays is even.
+
+// Example 1:
+// // Input: nums = [10,10,3,7,6]
+// Output: 4
+// // Explanation:
+// The 4 partitions are:
+
+// [10], [10, 3, 7, 6] with a sum difference of 10 - 26 = -16, which is even.
+// [10, 10], [3, 7, 6] with a sum difference of 20 - 16 = 4, which is even.
+// [10, 10, 3], [7, 6] with a sum difference of 23 - 13 = 10, which is even.
+// [10, 10, 3, 7], [6] with a sum difference of 30 - 6 = 24, which is even.
+// Example 2:
+// // Input: nums = [1,2,2]
+// Output: 0
+// // Explanation:
+// No partition results in an even sum difference.
+
+// Example 3:
+// Input: nums = [2,4,6,8]
+// Output: 3
+// Explanation:
+// All partitions result in an even sum difference
+
+const countParitions = (nums) => {
+  let countValidParitions = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    const leftSum = nums
+      .slice(0, i + 1)
+      .reduce((sum, current) => sum + current, 0);
+    const rightSum = nums
+      .slice(i + 1)
+      .reduce((sum, current) => sum + current, 0);
+    const sumDifference = leftSum - rightSum;
+    if (sumDifference % 2 === 0) {
+      countValidParitions++;
+    }
+  }
+  return countValidParitions;
+};
+console.log(countParitions([10, 10, 3, 7, 6]));
+console.log(countParitions([1, 2, 2]));
