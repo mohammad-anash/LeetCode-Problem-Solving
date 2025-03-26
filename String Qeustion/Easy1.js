5a858e766bbe2e9775f26fef7a1b4fffd231073b
mohammad-anash <anash40678910@gmail.com>
2025-03-26 13:24:54 +0530

    Updated Files

--- String Qeustion/Easy1.js
+++ String Qeustion/Easy1.js
@@ -172,6 +172,39 @@ const convertToTitle = (columnNumber) => {
   return result;
 };
 
-console.log(convertToTitle(1));
-console.log(convertToTitle(28));
-console.log(convertToTitle(701));
+// console.log(convertToTitle(1));
+// console.log(convertToTitle(28));
+// console.log(convertToTitle(701));
+
+// 134 => Add Digits
+
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+// Example 1:
+
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2
+// Since 2 has only one digit, return it.
+// Example 2:
+
+// Input: num = 0
+// Output: 0
+
+const addDigits = (x) => {
+  let inString = String(x);
+
+  while (inString.length !== 1) {
+    let sum = 0;
+    for (let i = 0; i < inString.length; i++) {
+      sum += +inString[i];
+    }
+    inString = String(sum);
+  }
+  return +inString;
+};
+
+// console.log(addDigits(38));
+// console.log(addDigits(0));
