9558271a4252ba2b06e91710d42058e348f62107
mohammad-anash <anash40678910@gmail.com>
2025-03-28 12:05:14 +0530

    Upated Files

--- String Qeustion/Easy1.js
+++ String Qeustion/Easy1.js
@@ -209,4 +209,49 @@ const addDigits = (x) => {
 // console.log(addDigits(38));
 // console.log(addDigits(0));
 
-console.log('update something');
+// 135 => find Special substring of length k
+
+// You are given a string s and an integer k.
+// Determine if there exists a substring of length exactly k in s that satisfies the following conditions:
+// The substring consists of only one distinct character (e.g., "aaa" or "bbb").
+// If there is a character immediately before the substring, it must be different from the character in the substring.
+// If there is a character immediately after the substring, it must also be different from the character in the substring.
+// Return true if such a substring exists. Otherwise, return false.
+
+// Example 1:
+// Input: s = "aaabaaa", k = 3
+// Output: true
+// Explanation:
+// The substring s[4..6] == "aaa" satisfies the conditions.
+
+// It has a length of 3.
+// All characters are the same.
+// The character before "aaa" is 'b', which is different from 'a'.
+// There is no character after "aaa".
+
+// Example 2:
+// Input: s = "abc", k = 2
+// Output: false
+// Explanation:
+// There is no substring of length 2 that consists of one distinct character and satisfies the conditions.
+
+const hasSpecialSubstring = (s, k) => {
+  for (let i = 0; i <= s.length - k; i++) {
+    const substring = s.slice(i, i + k);
+    const allSame = new Set(substring).size === 1;
+
+    if (allSame && substring.length === k) {
+      const beforeValid = s[i - 1] !== substring[0];
+
+      const afterValid = s[i + k] !== substring[0];
+
+      if (beforeValid && afterValid) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+console.log(hasSpecialSubstring('aaabaaa', 3));
+console.log(hasSpecialSubstring('abc', 2));
