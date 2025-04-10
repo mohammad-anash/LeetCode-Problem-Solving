3411fdb02ed6e10b7a81f87d9047d8d0657fc4f4
mohammad-anash <anash40678910@gmail.com>
2025-04-10 23:12:26 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -138,5 +138,59 @@ const isArraySpecial = (nums, queries) => {
 const isPalimdrom = (x) => {
   return +x.toString().split('').reverse().join('') === x;
 };
-console.log(isPalimdrom(121));
-console.log(isPalimdrom(123));
+
+// console.log(isPalimdrom(121));
+// console.log(isPalimdrom(123));
+
+// 376 => Maximum Value of an Ordered Triplet II
+
+// You are given a 0-indexed integer array nums.
+// Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+// The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+
+// Example 1:
+
+// Input: nums = [12,6,1,2,7]
+// Output: 77
+// Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+// It can be shown that there are no ordered triplets of indices with a value greater than 77.
+// Example 2:
+
+// Input: nums = [1,10,3,4,19]
+// Output: 133
+// Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+// It can be shown that there are no ordered triplets of indices with a value greater than 133.
+// Example 3:
+
+// Input: nums = [1,2,3]
+// Output: 0
+// Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+//
+function maximumTripletValue(nums) {
+  const n = nums.length;
+  if (n < 3) return 0;
+
+  const max_Left = new Array(n).fill(0);
+  max_Left[0] = -Infinity;
+  for (let i = 1; i < n; i++) {
+    max_Left[i] = Math.max(max_Left[i - 1], nums[i - 1]);
+  }
+
+  const max_Right = new Array(n).fill(0);
+  max_Right[n - 1] = -Infinity;
+  for (let j = n - 2; j >= 0; j--) {
+    max_Right[j] = Math.max(max_Right[j + 1], nums[j + 1]);
+  }
+
+  let maxTriplet = 0;
+  for (let j = 1; j < n - 1; j++) {
+    const current = (max_Left[j] - nums[j]) * max_Right[j];
+    maxTriplet = Math.max(maxTriplet, current);
+  }
+
+  return maxTriplet > 0 ? maxTriplet : 0;
+}
+
+// console.log(maximumTripletValue([12, 6, 1, 2, 7]));
+// console.log(maximumTripletValue([1, 10, 3, 4, 19]));
+// console.log(maximumTripletValue([1, 2, 3]));
