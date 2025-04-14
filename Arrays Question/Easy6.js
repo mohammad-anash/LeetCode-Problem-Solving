d382a2c8f502cdb48ec29b9ebcd7ea61d1f6b858
mohammad-anash <anash40678910@gmail.com>
2025-04-14 23:25:53 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -369,3 +369,45 @@ const valueAfterKSeconds = (n, k) => {
 };
 
 // console.log(valueAfterKSeconds(4, 5));
+
+// 381 => Find the Count of Numbers Which Are Not Special
+
+// You are given 2 positive integers l and r. For any number x, all positive divisors of x except x are called the proper divisors of x.
+
+// A number is called special if it has exactly 2 proper divisors. For example:
+
+// The number 4 is special because it has proper divisors 1 and 2.
+// The number 6 is not special because it has proper divisors 1, 2, and 3.
+// Return the count of numbers in the range [l, r] that are not special.
+
+// Example 1:
+// // Input: l = 5, r = 7
+// Output: 3
+// // Explanation:
+// There are no special numbers in the range [5, 7].
+
+// Example 2:
+// // Input: l = 4, r = 16
+// Output: 11
+// // Explanation:
+// The special numbers in the range [4, 16] are 4 and 9.
+
+const nonSpecialCount = (l, r) => {
+  let count = 0;
+
+  for (let i = l; i <= r; i++) {
+    let properDivisor = 0;
+    for (let j = 1; j < i; j++) {
+      if (i % j === 0) {
+        properDivisor++;
+      }
+    }
+    if (properDivisor !== 2) {
+      count++;
+    }
+  }
+  return count;
+};
+
+// console.log(nonSpecialCount(5, 7));
+// console.log(nonSpecialCount(4, 16));
