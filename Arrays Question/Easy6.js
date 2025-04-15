a0bc2db18041ca5f54722952228c7430f2dd812c
mohammad-anash <anash40678910@gmail.com>
2025-04-15 16:21:19 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -411,3 +411,51 @@ const nonSpecialCount = (l, r) => {
 
 // console.log(nonSpecialCount(5, 7));
 // console.log(nonSpecialCount(4, 16));
+
+// 382 =>  Reverse Degree of a String
+
+// Given a string s, calculate its reverse degree.
+// The reverse degree is calculated as follows:
+
+// For each character, multiply its position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1) with its position in the string (1-indexed).
+// Sum these products for all characters in the string.
+// Return the reverse degree of s.
+
+// Input: s = "abc"
+// Output: 148
+// Explanation:
+// Letter	Index in Reversed Alphabet	Index in String	Product
+// 'a'	26	1	26
+// 'b'	25	2	50
+// 'c'	24	3	72
+// The reversed degree is 26 + 50 + 72 = 148.
+
+// Example 2:
+// Input: s = "zaza"
+// Output: 160
+// Explanation:
+
+// Letter	Index in Reversed Alphabet	Index in String	Product
+// 'z'	1	1	1
+// 'a'	26	2	52
+// 'z'	1	3	3
+// 'a'	26	4	104
+// The reverse degree is 1 + 52 + 3 + 104 = 160
+
+const reverseDegree = (s) => {
+  let alphabets = '';
+  let result = 0;
+
+  for (let i = 122; i >= 97; i--) {
+    alphabets += String.fromCharCode(i);
+  }
+
+  for (let j = 0; j < s.length; j++) {
+    const findIndex = alphabets.indexOf(s[j]) + 1;
+    result += findIndex * (j + 1);
+  }
+  return result;
+};
+
+// console.log(reverseDegree('abc'));
+// console.log(reverseDegree('zaza'));
