726ac83c6d536288b7c40c0d5dcc8e8a9ed367b4
mohammad-anash <anash40678910@gmail.com>
2025-04-17 02:01:01 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -483,3 +483,39 @@ function myPow(x, n) {
 }
 
 // console.log(myPow(2.0, 10));
+
+// 384 => Count Square Sum Triples
+
+// A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+// Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+// Example 1:
+
+// Input: n = 5
+// Output: 2
+// // Explanation: The square triples are (3,4,5) and (4,3,5).
+// Example 2:
+
+// Input: n = 10
+// Output: 4
+// Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+
+function countSquareTriples(n) {
+  let count = 0;
+
+  for (let a = 1; a <= n; a++) {
+    for (let b = 1; b <= n; b++) {
+      const cSquared = a * a + b * b;
+      const c = Math.sqrt(cSquared);
+
+      if (c === Math.floor(c) && c <= n) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+// console.log(countTriples(5));
+// console.log(countTriples(10));
