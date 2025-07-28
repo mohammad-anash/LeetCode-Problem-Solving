50d239da032a4cebf16884cf2562eb6c1870e356
mohammad-anash <anash40678910@gmail.com>
2025-07-28 19:41:05 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -918,3 +918,44 @@ function diagonalSum(mat) {
 //   ])
 // ); // Output: 8
 // console.log(diagonalSum([[5]])); // Output: 5
+
+// 393 => Replace Elements with Greatest Element on Right Side
+
+// Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+// After doing so, return the array.
+
+// Example 1:
+
+// Input: arr = [17,18,5,4,6,1]
+// Output: [18,6,6,6,1,-1]
+// Explanation:
+// - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+// - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+// - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+// - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+// - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+// - index 5 --> there are no elements to the right of index 5, so we put -1.
+// Example 2:
+
+// Input: arr = [400]
+// Output: [-1]
+// Explanation: There are no elements to the right of index 0.
+
+const replaceElements = (nums) => {
+  const res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let max = -1;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] > max) {
+        max = nums[j];
+      }
+    }
+    res.push(max);
+  }
+  return res;
+};
+
+// console.log(replaceElements([17, 18, 5, 4, 6, 1]));
+// console.log(replaceElements([400]));
