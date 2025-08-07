40f1784dc580466e13954cfaadca95a6ff881a18
mohammad-anash <anash40678910@gmail.com>
2025-08-07 16:25:49 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -976,31 +976,46 @@ const replaceElements = (nums) => {
 // console.log(replaceElements([17, 18, 5, 4, 6, 1]));
 // console.log(replaceElements([400]));
 
-const reverseVowel = (s) => {
-  const vowels = 'aeiouAEIOU';
-  const storeIndex = [];
-  let storeVowel = '';
+// 395 => Next Greater Element I
 
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.includes(s[i])) {
-      storeIndex.push(i);
-      storeVowel += s[i];
+// The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+// You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+// For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+// Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+// Example 1:
+
+// Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+// Output: [-1,3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+// - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+// Example 2:
+
+// Input: nums1 = [2,4], nums2 = [1,2,3,4]
+// Output: [3,-1]
+// Explanation: The next greater element for each value of nums1 is as follows:
+// - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+// - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+const nextGreaterElement = (nums) => {
+  const stack = [];
+  let res = [];
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    while (stack.length !== 0 && stack[stack.length - 1] <= nums[i]) {
+      stack.pop();
     }
-  }
 
-  storeVowel = storeVowel.split('').reverse();
-
-  let res = '';
-
-  for (let i = 0; i < s.length; i++) {
-    if (storeIndex.includes(i)) {
-      res += storeVowel[j++];
-    } else {
-      res += s[i];
-    }
+    res[i] = stack.length === 0 ? -1 : stack[stack.length - 1];
+    stack.push(nums[i]);
   }
 
   return res;
 };
 
-// console.log(reverseVowel('IceCreAm));
+// console.log(nextGreaterElement([2, 1, 2, 4, 3]));
