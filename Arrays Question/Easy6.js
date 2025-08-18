c6b3c97e139d6dfc78eeaee00fca51326c1d2fee
mohammad-anash <anash40678910@gmail.com>
2025-08-18 16:47:46 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1141,14 +1141,67 @@ const addDigits = (x) => {
 // console.log(addDigits(999)); // 9
 // console.log(addDigits(12345)); // 6
 
-const distinctPrimeFactors = (nums) => {
-  let product = 1;
+// 399 => Shortest Distance to Target String in a Circular Array
 
-  for (let i = 0; i < nums.length; i++) {
-    product *= nums[i];
+// You are given a 0-indexed circular string array words and a string target. A circular array means that the array's end connects to the array's beginning.
+
+// Formally, the next element of words[i] is words[(i + 1) % n] and the previous element of words[i] is words[(i - 1 + n) % n], where n is the length of words.
+// Starting from startIndex, you can move to either the next word or the previous word with 1 step at a time.
+
+// Return the shortest distance needed to reach the string target. If the string target does not exist in words, return -1.
+
+// Example 1:
+//
+// Input: words = ["hello","i","am","leetcode","hello"], target = "hello", startIndex = 1
+// Output: 1
+// Explanation: We start from index 1 and can reach "hello" by
+// - moving 3 units to the right to reach index 4.
+// - moving 2 units to the left to reach index 4.
+// - moving 4 units to the right to reach index 0.
+// - moving 1 unit to the left to reach index 0.
+// The shortest distance to reach "hello" is 1.
+// Example 2:
+
+// Input: words = ["a","b","leetcode"], target = "leetcode", startIndex = 0
+// Output: 1
+// Explanation: We start from index 0 and can reach "leetcode" by
+// - moving 2 units to the right to reach index 3.
+// - moving 1 unit to the left to reach index 3.
+// The shortest distance to reach "leetcode" is 1.
+// Example 3:
+//
+// Input: words = ["i","eat","leetcode"], target = "ate", startIndex = 0
+// Output: -1
+// Explanation: Since "ate" does not exist in words, we return -1.
+
+const ClosesTarget = (words, target, startIndex) => {
+  if (!words.includes(target)) return -1;
+
+  let n = words.length;
+  let left = Infinity,
+    right = Infinity;
+
+  for (let step = 0; step < n; step++) {
+    let i = (startIndex + step) % n;
+    if (words[i] === target) {
+      right = step;
+      break;
+    }
   }
 
-  return product;
+  for (let step = 0; step < n; step++) {
+    let i = (startIndex - step + n) % n;
+    if (words[i] === target) {
+      left = step;
+      break;
+    }
+  }
+
+  return Math.min(left, right);
 };
 
-console.log(distinctPrimeFactors([2, 4, 3, 7, 10, 6]
+console.log(
+  ClosesTarget(['hello', 'i', 'am', 'leetcode', 'hello'], 'hello', 1)
+); // 1
+console.log(ClosesTarget(['a', 'b', 'leetcode'], 'leetcode', 0)); // 1
+console.log(ClosesTarget(['i', 'eat', 'leetcode'], 'ate', 0)); // -1
