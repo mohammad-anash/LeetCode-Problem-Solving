dc53b7afa3d9ad139fb2589f32072c8f8f4b4bd0
mohammad-anash <anash40678910@gmail.com>
2025-08-23 23:24:20 +0530

    Updated FILES

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1411,29 +1411,74 @@ const sumOfMultiples = (n) => {
 
 // console.log(sumOfMultiples(7));
 
-const romanToInt = (str) => {
-  let res = 0;
+// 405 => 3Sum closest
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === 'I') {
-      res += 1;
-    } else if (str[i] === 'V') {
-      res += 5;
-    } else if (str[i] === 'X') {
-      res += 10;
-    } else if (str[i] === 'L') {
-      res += 50;
-    } else if (str[i] === 'C') {
-      res += 100;
-    } else if (str[i] === 'D') {
-      res += 500;
-    } else if (str[i] === 'M') {
-      res += 1000;
+// Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+// Return the sum of the three integers.
+// You may assume that each input would have exactly one solution.
+
+// Example 1:
+
+// Input: nums = [-1,2,1,-4], target = 1
+// Output: 2
+// Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+// Example 2:
+
+// Input: nums = [0,0,0], target = 1
+// Output: 0
+// Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+const threeSumClosest = (nums, target) => {
+  nums.sort((a, b) => a - b);
+  let closestSum = Infinity;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let currentSum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum < target) {
+        left++;
+      } else if (currentSum > target) {
+        right--;
+      } else {
+        return currentSum;
+      }
     }
   }
-  return res;
+  return closestSum;
 };
 
-// console.log(romanToInt('III'));
-// console.log(romanToInt('LVIII'));
-// console.log(romanToInt('MCMXCIV'));
+// console.log(threeSumClosest([-1, 2, 1, -4], 1));
+// console.log(threeSumClosest([0, 0, 0], 1));
+
+const threeSumMulti = (nums, target) => {
+  let countTouples = 0;
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+
+      if (sum < target) {
+        j++;
+      } else if (sum > target) {
+        k--;
+      } else {
+        countTouples++;
+      }
+    }
+  }
+
+  return countTouples;
+};
+
+console.log(threeSumMulti([1, 1, 2, 2, 3, 3, 4, 4, 5, 5], 8)
