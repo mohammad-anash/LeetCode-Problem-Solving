d47e4cb252fa9e52f0487cee4e550cf7d237e39f
mohammad-anash <anash40678910@gmail.com>
2025-08-28 22:45:17 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1456,4 +1456,4 @@ const threeSumClosest = (nums, target) => {
 };
 
 // console.log(threeSumClosest([-1, 2, 1, -4], 1));
-// console.log(threeSumClosest([0, 0, 0], 1));
+// console.log(threeSumClosest([0, 0, 0], 1)
