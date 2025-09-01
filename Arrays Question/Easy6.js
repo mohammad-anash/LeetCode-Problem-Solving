cbcf5b1590612f84d2cfa91f3491fea1b6b21d7a
mohammad-anash <anash40678910@gmail.com>
2025-09-01 20:34:40 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1531,3 +1531,50 @@ const getLeastFerquentDigit = (n) => {
 
 // console.log(getLeastFerquentDigit(1553322)); // 1
 // console.log(getLeastFerquentDigit(723344511)); // 2
+
+// 405 => Integer Replacement
+
+// Given a positive integer n, you can apply one of the following operations:
+
+// If n is even, replace n with n / 2.
+// If n is odd, replace n with either n + 1 or n - 1.
+// Return the minimum number of operations needed for n to become 1.
+
+// Example 1:
+
+// Input: n = 8
+// Output: 3
+// Explanation: 8 -> 4 -> 2 -> 1
+// Example 2:
+
+// Input: n = 7
+// Output: 4
+// Explanation: 7 -> 8 -> 4 -> 2 -> 1
+// or 7 -> 6 -> 3 -> 2 -> 1
+// Example 3:
+
+// Input: n = 4
+// Output: 2
+
+const integerReplacement = function (n) {
+  let count = 0;
+
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      if (n === 3 || (n & 2) === 0) {
+        n = n - 1;
+      } else {
+        n = n + 1;
+      }
+    }
+    count++;
+  }
+
+  return count;
+};
+
+// console.log(integerReplacement(8));
+// console.log(integerReplacement(7));
+// console.log(integerReplacement(4));
