5148a38f1e8496e50a424456a848bb79b53783f8
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2025-09-15 00:17:28 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1661,3 +1661,48 @@ const maxKDistinct = (nums, k) => {
 // console.log(maxKDistinct([84, 93, 100, 77, 90], 3));
 // console.log(maxKDistinct([84, 93, 100, 77, 93], 3));
 // console.log(maxKDistinct([1, 1, 1, 2, 2, 2], 6));
+
+// 408 => Earliest Time to Finish One Task
+
+// You are given a 2D integer array tasks where tasks[i] = [si, ti].
+// Each [si, ti] in tasks represents a task with start time si that takes ti units of time to finish.
+// Return the earliest time at which at least one task is finished.
+
+// Example 1:
+// Input: tasks = [[1,6],[2,3]]
+// Output: 5
+// Explanation:
+
+// The first task starts at time t = 1 and finishes at time 1 + 6 = 7. The second task finishes at time 2 + 3 = 5. You can finish one task at time 5.
+
+// Example 2:
+// Input: tasks = [[100,100],[100,100],[100,100]]
+// Output: 200
+// Explanation:
+
+// All three tasks finish at time 100 + 100 = 200
+
+const earliestTime = (nums) => {
+  const times = [];
+
+  for (let [x1, x2] of nums) {
+    const time = x1 + x2;
+    times.push(time);
+  }
+
+  return Math.min(...times);
+};
+
+// console.log(
+//   earliestTime([
+//     [1, 6],
+//     [2, 3],
+//   ])
+// );
+// console.log(
+//   earliestTime([
+//     [100, 100],
+//     [100, 100],
+//     [100, 100],
+//   ])
+// );
