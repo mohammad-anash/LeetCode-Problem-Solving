9872be02d834f5f8835fd788bb60f06f106c7b02
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2025-11-09 22:36:03 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1792,4 +1792,4 @@ const findMissingElements = (nums) => {
 
 // console.log(findMissingElements([1, 4, 2, 5]));
 // console.log(findMissingElements([7, 8, 6, 9]));
-// console.log(findMissingElements([5, 8, 7]])
+// console.log(findMissingElements([5, 8,
