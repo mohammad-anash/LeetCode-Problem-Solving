a61296385db0fe708921dd5344a78cf2b6690fb9
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2025-11-19 21:51:06 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1794,19 +1794,46 @@ const findMissingElements = (nums) => {
 // console.log(findMissingElements([7, 8, 6, 9]));
 // console.log(findMissingElements([5, 8,]));
 
-const numSub = (s) => {
-  let res = 0;
+// 420 => Keep Multiplying Found Values by Two
 
-  for (let i = 0; i < s.length - 1; i++) {
-    for (let j = i; j <= s.length; j++) {
-      const countStr = s.slice(i, j);
+// You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+// You then do the following steps:
+// If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+// Otherwise, stop the process.
+// Repeat this process with the new number as long as you keep finding the number.
+// Return the final value of original.
 
-      if (!countStr.includes('0')) res++;
+// Example 1:
+
+// Input: nums = [5,3,6,1,12], original = 3
+// Output: 24
+// Explanation:
+// - 3 is found in nums. 3 is multiplied by 2 to obtain 6.
+// - 6 is found in nums. 6 is multiplied by 2 to obtain 12.
+// - 12 is found in nums. 12 is multiplied by 2 to obtain 24.
+// - 24 is not found in nums. Thus, 24 is returned.
+// Example 2:
+
+// Input: nums = [2,7,9], original = 4
+// Output: 4
+// Explanation:
+// - 4 is not found in nums. Thus, 4 is returned.
+
+const findFinalValue = (nums, original) => {
+  let value = original;
+  let newValue = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.includes(value)) {
+      newValue = value * 2;
+
+      value = newValue;
+    } else {
+      break;
     }
   }
-
-  return res;
+  return value;
 };
 
-console.log(numSub('0110111'));
-// console.log(numSub('10));
+console.log(findFinalValue([5, 3, 6, 1, 12], 3));
+console.log(findFinalValue([2, 7, 9], 4));
