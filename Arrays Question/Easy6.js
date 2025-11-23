d23122b37709813e78ab1847254df305e089fe2b
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2025-11-23 22:46:40 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1836,4 +1836,4 @@ const findFinalValue = (nums, original) => {
 };
 
 console.log(findFinalValue([5, 3, 6, 1, 12], 3));
-console.log(findFinalValue([2, 7, 9], 4)
+console.log(findFinalValue([2, 7, 9], 4));
