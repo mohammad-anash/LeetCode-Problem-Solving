6af62d2c6193aaa9901d19c471a364aa9929b8ba
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2025-11-24 20:27:55 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -1835,5 +1835,35 @@ const findFinalValue = (nums, original) => {
   return value;
 };
 
-console.log(findFinalValue([5, 3, 6, 1, 12], 3));
-console.log(findFinalValue([2, 7, 9], 4));
+// console.log(findFinalValue([5, 3, 6, 1, 12], 3));
+// console.log(findFinalValue([2, 7, 9], 4));
+
+// 421 => Maximum XOR of Two Numbers in an Array
+
+// Given an integer array nums, return the maximum result of nums[i] XOR nums[j], where 0 <= i <= j < n.
+
+// Example 1:
+
+// Input: nums = [3,10,5,25,2,8]
+// Output: 28
+// Explanation: The maximum result is 5 XOR 25 = 28.
+// Example 2:
+
+// Input: nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+// Output: 127
+
+const findMaximumXOR = (nums) => {
+  let maxXOR = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      maxXOR = Math.max(nums[i] ^ nums[j], maxXOR);
+    }
+  }
+
+  return maxXOR;
+};
+
+// console.log(findMaximumXOR([3, 10, 5, 25, 2, 8]));
+// console.log(findMaximumXOR([14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70]));
+git add 
