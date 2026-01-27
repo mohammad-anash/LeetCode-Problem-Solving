0b9de6b4454b830371440464603d309ed569631d
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2026-01-27 15:48:33 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -2536,3 +2536,41 @@ const countSubArrays = (arr) => {
 
 // console.log(countSubArrays([1, 2, 1, 4, 1]));
 // console.log(countSubArrays([1, 1, 1]));
+
+// 436 => Sum of Good Numbers
+
+// Given an array of integers nums and an integer k, an element nums[i] is considered good if it is strictly greater than the elements at indices i - k and i + k (if those indices exist). If neither of these indices exists, nums[i] is still considered good.
+
+// Return the sum of all the good elements in the array.
+
+// Example 1:
+// Input: nums = [1,3,2,1,5,4], k = 2
+// Output: 12
+// Explanation:
+// The good numbers are nums[1] = 3, nums[4] = 5, and nums[5] = 4 because they are strictly greater than the numbers at indices i - k and i + k.
+
+// Example 2:
+// Input: nums = [2,1], k = 1
+// Output: 2
+// Explanation:
+// The only good number is nums[0] = 2 because it is strictly greater than nums[1]
+
+const sumOffGoodNumbers = (arr, k) => {
+  let goodNumSum = 0;
+
+  function isGoodIndex(index, current) {
+    if (index < 0 || index >= arr.length) return true;
+    return current > arr[index];
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (isGoodIndex(i - k, arr[i]) && isGoodIndex(i + k, arr[i])) {
+      goodNumSum += arr[i];
+    }
+  }
+
+  return goodNumSum;
+};
+
+// console.log(sumOffGoodNumbers([1, 3, 2, 1, 5, 4], 2));
+// console.log(sumOffGoodNumbers([2, 1], 1));
