7908eb568fc5ae6ef8984d0cad6c7b1e6ab9b7e1
mohammad-anash <132842376+mohammad-anash@users.noreply.github.com>
2026-01-30 12:11:51 +0530

    Updated Files

--- Arrays Question/Easy6.js
+++ Arrays Question/Easy6.js
@@ -2708,3 +2708,51 @@ const smallestIndex = (nums) => {
 // console.log(smallestIndex([1, 10, 11]));
 // console.log(smallestIndex([1, 3, 2]));
 // console.log(smallestIndex([1, 2, 3]));
+
+// 440 => Greatest English Letter in Upper and Lower Case
+
+// Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. The returned letter should be in uppercase. If no such letter exists, return an empty string.
+
+// An English letter b is greater than another letter a if b appears after a in the English alphabet.
+
+// Example 1:
+
+// Input: s = "lEeTcOdE"
+// Output: "E"
+// Explanation:
+// The letter 'E' is the only letter to appear in both lower and upper case.
+// Example 2:
+
+// Input: s = "arRAzFif"
+// Output: "R"
+// Explanation:
+// // The letter 'R' is the greatest letter to appear in both lower and upper case.
+// // Note that 'A' and 'F' also appear in both lower and upper case, but 'R' is greater than 'F' or 'A'.
+// Example 3:
+
+// Input: s = "AbCdEfGhIjK"
+// Output: ""
+// Explanation:
+// There is no letter that appears in both lower and upper case.
+
+const greatestLetter = (s) => {
+  let appearLetter = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const upperCaseLetter = s[i].toUpperCase();
+    const lowerCaseLetter = s[i].toLowerCase();
+
+    if (s.includes(upperCaseLetter) && s.includes(lowerCaseLetter)) {
+      appearLetter.push(upperCaseLetter);
+    }
+  }
+
+  if (appearLetter.length === 0) return '';
+
+  appearLetter.sort();
+  return appearLetter[appearLetter.length - 1];
+};
+
+// console.log(greatestLetter('lEeTcOdE')); // E
+// console.log(greatestLetter('arRAzFif')); // R
+// console.log(greatestLetter('AbCdEfGhIjK')); // ""
